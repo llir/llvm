907bcba22a4386c8f6bc6d1265a6b95fdbbadce6
mewmew <rnd0x00@gmail.com>
2015-01-15 23:43:21 +0100

    asm/lexer: Add third test case. Updates #8.

--- asm/lexer/lexer_test.go
+++ asm/lexer/lexer_test.go
@@ -28,6 +28,20 @@ func TestParse(t *testing.T) {
 				{Kind: token.EOF, Line: 1, Col: 10},
 			},
 		},
+		// i=2
+		{
+			input: "@foo%bar$baz!qux@42%37#7",
+			want: []token.Token{
+				{Kind: token.GlobalVar, Val: "@foo", Line: 1, Col: 1},
+				{Kind: token.LocalVar, Val: "%bar", Line: 1, Col: 5},
+				{Kind: token.ComdatVar, Val: "$baz", Line: 1, Col: 9},
+				{Kind: token.MetadataVar, Val: "!qux", Line: 1, Col: 13},
+				{Kind: token.GlobalID, Val: "@42", Line: 1, Col: 17},
+				{Kind: token.LocalID, Val: "%37", Line: 1, Col: 20},
+				{Kind: token.AttrID, Val: "#7", Line: 1, Col: 23},
+				{Kind: token.EOF, Line: 1, Col: 25},
+			},
+		},
 	}
 	for i, g := range golden {
 		got := Parse(g.input)
