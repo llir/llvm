c02f24318821319a6468d529cf2755f841284ade
mewmew <rnd0x00@gmail.com>
2015-01-16 19:30:09 +0100

    asm/lexer: Add lookup table for keywords and corresponding test case. Updates issue #8.

--- asm/token/token.go
+++ asm/token/token.go
@@ -71,7 +71,86 @@ const (
 
 	keywordStart
 
+	instructionStart
+
 	// Keywords.
+	// Instructions.
+	// Terminator instructions.
+	KwRet         // ret
+	KwBr          // br
+	KwSwitch      // switch
+	KwIndirectbr  // indirectbr
+	KwInvoke      // invoke
+	KwResume      // resume
+	KwUnreachable // unreachable
+
+	// Binary operations.
+	KwAdd  // add
+	KwFadd // fadd
+	KwSub  // sub
+	KwFsub // fsub
+	KwMul  // mul
+	KwFmul // fmul
+	KwUdiv // udiv
+	KwSdiv // sdiv
+	KwFdiv // fdiv
+	KwUrem // urem
+	KwSrem // srem
+	KwFrem // frem
+
+	// Bitwise binary operations.
+	KwShl  // shl
+	KwLshr // lshr
+	KwAshr // ashr
+	KwAnd  // and
+	KwOr   // or
+	KwXor  // xor
+
+	// Vector operations.
+	KwExtractelement // extractelement
+	KwInsertelement  // insertelement
+	KwShufflevector  // shufflevector
+
+	// Aggregate operations.
+	KwExtractvalue // extractvalue
+	KwInsertvalue  // insertvalue
+
+	// Memory access and addressing operations.
+	KwAlloca        // alloca
+	KwLoad          // load
+	KwStore         // store
+	KwFence         // fence
+	KwCmpxchg       // cmpxchg
+	KwAtomicrmw     // atomicrmw
+	KwGetelementptr // getelementptr
+
+	// Conversion operations.
+	KwTo            // to
+	KwTrunc         // trunc
+	KwZext          // zext
+	KwSext          // sext
+	KwFptrunc       // fptrunc
+	KwFpext         // fpext
+	KwFptoui        // fptoui
+	KwFptosi        // fptosi
+	KwUitofp        // uitofp
+	KwSitofp        // sitofp
+	KwPtrtoint      // ptrtoint
+	KwInttoptr      // inttoptr
+	KwBitcast       // bitcast
+	KwAddrspacecast // addrspacecast
+
+	// Other operations.
+	KwIcmp       // icmp
+	KwFcmp       // fcmp
+	KwPhi        // phi
+	KwSelect     // select
+	KwCall       // call
+	KwVaArg      // va_arg
+	KwLandingpad // landingpad
+
+	instructionEnd
+
 	KwNull     // null
 	KwTrue     // true
 	KwFalse    // false
@@ -262,85 +341,6 @@ const (
 	// Addresses of basic blocks.
 	KwBlockaddress // blockaddress
 
-	instructionStart
-
-	// Instructions.
-	// Terminator instructions.
-	KwRet         // ret
-	KwBr          // br
-	KwSwitch      // switch
-	KwIndirectbr  // indirectbr
-	KwInvoke      // invoke
-	KwResume      // resume
-	KwUnreachable // unreachable
-
-	// Binary operations.
-	KwAdd  // add
-	KwFadd // fadd
-	KwSub  // sub
-	KwFsub // fsub
-	KwMul  // mul
-	KwFmul // fmul
-	KwUdiv // udiv
-	KwSdiv // sdiv
-	KwFdiv // fdiv
-	KwUrem // urem
-	KwSrem // srem
-	KwFrem // frem
-
-	// Bitwise binary operations.
-	KwShl  // shl
-	KwLshr // lshr
-	KwAshr // ashr
-	KwAnd  // and
-	KwOr   // or
-	KwXor  // xor
-
-	// Vector operations.
-	KwExtractelement // extractelement
-	KwInsertelement  // insertelement
-	KwShufflevector  // shufflevector
-
-	// Aggregate operations.
-	KwExtractvalue // extractvalue
-	KwInsertvalue  // insertvalue
-
-	// Memory access and addressing operations.
-	KwAlloca        // alloca
-	KwLoad          // load
-	KwStore         // store
-	KwFence         // fence
-	KwCmpxchg       // cmpxchg
-	KwAtomicrmw     // atomicrmw
-	KwGetelementptr // getelementptr
-
-	// Conversion operations.
-	KwTo            // to
-	KwTrunc         // trunc
-	KwZext          // zext
-	KwSext          // sext
-	KwFptrunc       // fptrunc
-	KwFpext         // fpext
-	KwFptoui        // fptoui
-	KwFptosi        // fptosi
-	KwUitofp        // uitofp
-	KwSitofp        // sitofp
-	KwPtrtoint      // ptrtoint
-	KwInttoptr      // inttoptr
-	KwBitcast       // bitcast
-	KwAddrspacecast // addrspacecast
-
-	// Other operations.
-	KwIcmp       // icmp
-	KwFcmp       // fcmp
-	KwPhi        // phi
-	KwSelect     // select
-	KwCall       // call
-	KwVaArg      // va_arg
-	KwLandingpad // landingpad
-
-	instructionEnd
-
 	// Poison behaviour.
 	KwNuw      // nuw: No Unsigned Wrap
 	KwNsw      // nsw: No Signed Wrap
@@ -415,3 +415,254 @@ func (kind Kind) IsLiteral() bool {
 	return literalStart < kind && kind < literalEnd
 }
 */
+
+// Keywords is the set of valid keywords in LLVM IR assembly.
+var Keywords = map[string]Kind{
+	// Types.
+	"void":      Type,
+	"half":      Type,
+	"float":     Type,
+	"double":    Type,
+	"x86_fp80":  Type,
+	"fp128":     Type,
+	"ppc_fp128": Type,
+	"x86_mmx":   Type,
+	"label":     Type,
+	"metadata":  Type,
+
+	// Instructions.
+	"ret":            KwRet,
+	"br":             KwBr,
+	"switch":         KwSwitch,
+	"indirectbr":     KwIndirectbr,
+	"invoke":         KwInvoke,
+	"resume":         KwResume,
+	"unreachable":    KwUnreachable,
+	"add":            KwAdd,
+	"fadd":           KwFadd,
+	"sub":            KwSub,
+	"fsub":           KwFsub,
+	"mul":            KwMul,
+	"fmul":           KwFmul,
+	"udiv":           KwUdiv,
+	"sdiv":           KwSdiv,
+	"fdiv":           KwFdiv,
+	"urem":           KwUrem,
+	"srem":           KwSrem,
+	"frem":           KwFrem,
+	"shl":            KwShl,
+	"lshr":           KwLshr,
+	"ashr":           KwAshr,
+	"and":            KwAnd,
+	"or":             KwOr,
+	"xor":            KwXor,
+	"extractelement": KwExtractelement,
+	"insertelement":  KwInsertelement,
+	"shufflevector":  KwShufflevector,
+	"extractvalue":   KwExtractvalue,
+	"insertvalue":    KwInsertvalue,
+	"alloca":         KwAlloca,
+	"load":           KwLoad,
+	"store":          KwStore,
+	"fence":          KwFence,
+	"cmpxchg":        KwCmpxchg,
+	"atomicrmw":      KwAtomicrmw,
+	"getelementptr":  KwGetelementptr,
+	"to":             KwTo,
+	"trunc":          KwTrunc,
+	"zext":           KwZext,
+	"sext":           KwSext,
+	"fptrunc":        KwFptrunc,
+	"fpext":          KwFpext,
+	"fptoui":         KwFptoui,
+	"fptosi":         KwFptosi,
+	"uitofp":         KwUitofp,
+	"sitofp":         KwSitofp,
+	"ptrtoint":       KwPtrtoint,
+	"inttoptr":       KwInttoptr,
+	"bitcast":        KwBitcast,
+	"addrspacecast":  KwAddrspacecast,
+	"icmp":           KwIcmp,
+	"fcmp":           KwFcmp,
+	"phi":            KwPhi,
+	"select":         KwSelect,
+	"call":           KwCall,
+	"va_arg":         KwVaArg,
+	"landingpad":     KwLandingpad,
+
+	// Keywords.
+	"null":                   KwNull,
+	"true":                   KwTrue,
+	"false":                  KwFalse,
+	"x":                      KwX,
+	"declare":                KwDeclare,
+	"define":                 KwDefine,
+	"global":                 KwGlobal,
+	"constant":               KwConstant,
+	"private":                KwPrivate,
+	"internal":               KwInternal,
+	"available_externally":   KwAvailableExternally,
+	"linkonce":               KwLinkonce,
+	"weak":                   KwWeak,
+	"common":                 KwCommon,
+	"appending":              KwAppending,
+	"extern_weak":            KwExternWeak,
+	"linkonce_odr":           KwLinkonceOdr,
+	"weak_odr":               KwWeakOdr,
+	"external":               KwExternal,
+	"ccc":                    KwCcc,
+	"fastcc":                 KwFastcc,
+	"coldcc":                 KwColdcc,
+	"webkit_jscc":            KwWebkitJscc,
+	"anyregcc":               KwAnyregcc,
+	"preserve_mostcc":        KwPreserveMostcc,
+	"preserve_allcc":         KwPreserveAllcc,
+	"cc":                     KwCc,
+	"intel_ocl_bicc":         KwIntelOclBicc,
+	"x86_stdcallcc":          KwX86Stdcallcc,
+	"x86_fastcallcc":         KwX86Fastcallcc,
+	"x86_thiscallcc":         KwX86Thiscallcc,
+	"x86_vectorcallcc":       KwX86Vectorcallcc,
+	"arm_apcscc":             KwArmApcscc,
+	"arm_aapcscc":            KwArmAapcscc,
+	"arm_aapcs_vfpcc":        KwArmAapcsVfpcc,
+	"msp430_intrcc":          KwMsp430Intrcc,
+	"ptx_kernel":             KwPtxKernel,
+	"ptx_device":             KwPtxDevice,
+	"spir_kernel":            KwSpirKernel,
+	"spir_func":              KwSpirFunc,
+	"x86_64_sysvcc":          KwX86_64Sysvcc,
+	"x86_64_win64cc":         KwX86_64Win64cc,
+	"ghccc":                  KwGhccc,
+	"default":                KwDefault,
+	"hidden":                 KwHidden,
+	"protected":              KwProtected,
+	"dllimport":              KwDllimport,
+	"dllexport":              KwDllexport,
+	"localdynamic":           KwLocaldynamic,
+	"initialexec":            KwInitialexec,
+	"localexec":              KwLocalexec,
+	"thread_local":           KwThreadLocal,
+	"unnamed_addr":           KwUnnamedAddr,
+	"addrspace":              KwAddrspace,
+	"externally_initialized": KwExternallyInitialized,
+	"section":                KwSection,
+	"prologue":               KwPrologue,
+	"alias":                  KwAlias,
+	"comdat":                 KwComdat,
+	"any":                    KwAny,
+	"exactmatch":             KwExactmatch,
+	"largest":                KwLargest,
+	"noduplicates":           KwNoduplicates,
+	"samesize":               KwSamesize,
+	"zeroext":                KwZeroext,
+	"signext":                KwSignext,
+	"inreg":                  KwInreg,
+	"byval":                  KwByval,
+	"inalloca":               KwInalloca,
+	"sret":                   KwSret,
+	"align":                  KwAlign,
+	"noalias":                KwNoalias,
+	"nocapture":              KwNocapture,
+	"nest":                   KwNest,
+	"returned":               KwReturned,
+	"nonnull":                KwNonnull,
+	"dereferenceable":        KwDereferenceable,
+	"gc":                     KwGc,
+	"prefix":                 KwPrefix,
+	"attributes":             KwAttributes,
+	"alignstack":             KwAlignstack,
+	"alwaysinline":           KwAlwaysinline,
+	"builtin":                KwBuiltin,
+	"cold":                   KwCold,
+	"inlinehint":             KwInlinehint,
+	"jumptable":              KwJumptable,
+	"minsize":                KwMinsize,
+	"naked":                  KwNaked,
+	"nobuiltin":              KwNobuiltin,
+	"noduplicate":            KwNoduplicate,
+	"noimplicitfloat":        KwNoimplicitfloat,
+	"noinline":               KwNoinline,
+	"nonlazybind":            KwNonlazybind,
+	"noredzone":              KwNoredzone,
+	"noreturn":               KwNoreturn,
+	"nounwind":               KwNounwind,
+	"optnone":                KwOptnone,
+	"optsize":                KwOptsize,
+	"readnone":               KwReadnone,
+	"readonly":               KwReadonly,
+	"returns_twice":          KwReturnsTwice,
+	"sanitize_address":       KwSanitizeAddress,
+	"sanitize_memory":        KwSanitizeMemory,
+	"sanitize_thread":        KwSanitizeThread,
+	"ssp":                    KwSsp,
+	"sspreq":                 KwSspreq,
+	"sspstrong":              KwSspstrong,
+	"uwtable":                KwUwtable,
+	"module":                 KwModule,
+	"asm":                    KwAsm,
+	"sideeffect":             KwSideeffect,
+	"inteldialect":           KwInteldialect,
+	"target":                 KwTarget,
+	"datalayout":             KwDatalayout,
+	"triple":                 KwTriple,
+	"volatile":               KwVolatile,
+	"c":                      KwC,
+	"zeroinitializer":        KwZeroinitializer,
+	"unordered":              KwUnordered,
+	"monotonic":              KwMonotonic,
+	"acquire":                KwAcquire,
+	"release":                KwRelease,
+	"acq_rel":                KwAcqRel,
+	"seq_cst":                KwSeqCst,
+	"xchg":                   KwXchg,
+	"nand":                   KwNand,
+	"max":                    KwMax,
+	"min":                    KwMin,
+	"umax":                   KwUmax,
+	"umin":                   KwUmin,
+	"nnan":                   KwNnan,
+	"ninf":                   KwNinf,
+	"nsz":                    KwNsz,
+	"arcp":                   KwArcp,
+	"fast":                   KwFast,
+	"uselistorder":           KwUselistorder,
+	"uselistorder_bb":        KwUselistorderBb,
+	"type":                   KwType,
+	"opaque":                 KwOpaque,
+	"undef":                  KwUndef,
+	"blockaddress":           KwBlockaddress,
+	"nuw":                    KwNuw,
+	"nsw":                    KwNsw,
+	"exact":                  KwExect,
+	"inbounds":               KwInbounds,
+	"atomic":                 KwAtomic,
+	"singlethread":           KwSinglethread,
+	"eq":                     KwEq,
+	"ne":                     KwNe,
+	"ugt":                    KwUgt,
+	"uge":                    KwUge,
+	"ult":                    KwUlt,
+	"ule":                    KwUle,
+	"sgt":                    KwSgt,
+	"sge":                    KwSge,
+	"slt":                    KwSlt,
+	"sle":                    KwSle,
+	"oeq":                    KwOeq,
+	"ogt":                    KwOgt,
+	"oge":                    KwOge,
+	"olt":                    KwOlt,
+	"ole":                    KwOle,
+	"one":                    KwOne,
+	"ord":                    KwOrd,
+	"ueq":                    KwUeq,
+	"une":                    KwUne,
+	"uno":                    KwUno,
+	"tail":                   KwTail,
+	"musttail":               KwMusttail,
+	"personality":            KwPersonality,
+	"cleanup":                KwCleanup,
+	"catch":                  KwCatch,
+	"filter":                 KwFilter,
+	"unwind":                 KwUnwind,
+}
