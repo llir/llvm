a56cc1db2058cdac34da65ae29056c1b75ac0fcd
mewmew <rnd0x00@gmail.com>
2015-01-18 11:34:03 +0100

    asm/lexer: Add ParseString example.

--- asm/lexer/example_test.go
+++ asm/lexer/example_test.go
@@ -31,3 +31,64 @@ func ExampleParse() {
 	// GlobalVar    "p"
 	// Equal        "="
 }
+
+func ExampleParseString() {
+	tokens := lexer.ParseString("%bar = icmp sge i32 %foo, 65 ; bar = (foo >= 'A')")
+	for i, tok := range tokens {
+		fmt.Printf("=== [ token %d ] ===\n", i)
+		fmt.Println("pos: ", tok.Pos)
+		fmt.Println("kind:", tok.Kind)
+		fmt.Println("val: ", tok.Val)
+		fmt.Println()
+	}
+	// Output:
+	// === [ token 0 ] ===
+	// pos:  0
+	// kind: LocalVar
+	// val:  bar
+	//
+	// === [ token 1 ] ===
+	// pos:  5
+	// kind: Equal
+	// val:  =
+	//
+	// === [ token 2 ] ===
+	// pos:  7
+	// kind: KwIcmp
+	// val:  icmp
+	//
+	// === [ token 3 ] ===
+	// pos:  12
+	// kind: KwSge
+	// val:  sge
+	//
+	// === [ token 4 ] ===
+	// pos:  16
+	// kind: Type
+	// val:  i32
+	//
+	// === [ token 5 ] ===
+	// pos:  20
+	// kind: LocalVar
+	// val:  foo
+	//
+	// === [ token 6 ] ===
+	// pos:  24
+	// kind: Comma
+	// val:  ,
+	//
+	// === [ token 7 ] ===
+	// pos:  26
+	// kind: Int
+	// val:  65
+	//
+	// === [ token 8 ] ===
+	// pos:  29
+	// kind: Comment
+	// val:   bar = (foo >= 'A')
+	//
+	// === [ token 9 ] ===
+	// pos:  49
+	// kind: EOF
+	// val:
+}
