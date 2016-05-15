9def3d1593dbe492f91697cfd8d25134af128b3b
mewmew <rnd0x00@gmail.com>
2016-05-15 03:14:59 +0200

    ir: Unexport BasicBlock fields and add access through methods.

--- ir/basicblock.go
+++ ir/basicblock.go
@@ -12,11 +12,42 @@ import "github.com/llir/llvm/ir/instruction"
 //    http://llvm.org/docs/LangRef.html#terminators
 type BasicBlock struct {
 	// Basic block label name.
-	Name string
+	name string
 	// Parent function of the basic block.
-	Parent *Function
+	parent *Function
 	// Non-terminator instructions of the basic block.
-	Insts []instruction.Instruction
+	insts []instruction.Instruction
 	// Terminator instruction of the basic block.
-	Term instruction.Terminator
+	term instruction.Terminator
+}
+
+// NewBasicBlock returns a new basic block based on the given name, non-
+// terminating instructions and terminator.
+func NewBasicBlock(name string, insts []instruction.Instruction, term instruction.Terminator) *BasicBlock {
+	return &BasicBlock{name: name, insts: insts, term: term}
+}
+
+// Name returns the name of the basic block.
+func (block *BasicBlock) Name() string {
+	return block.name
+}
+
+// Parent returns the parent function of the basic block.
+func (block *BasicBlock) Parent() *Function {
+	return block.parent
+}
+
+// TODO: Add SetParent method to BasicBlock?
+
+// Insts returns the non-terminating instructions of the basic block.
+func (block *BasicBlock) Insts() []instruction.Instruction {
+	return block.insts
+}
+
+// TODO: Add AppendInst and SetInsts methods to BasicBlock? Analogously defined
+// as for Function.AppendBlock and Function.SetBlocks.
+
+// Term returns the terminator of the basic block.
+func (block *BasicBlock) Term() instruction.Terminator {
+	return block.term
 }
