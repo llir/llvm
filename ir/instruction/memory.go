3c589f3cda6e695cd9f87611e6d8e34c72828100
mewmew <rnd0x00@gmail.com>
2016-05-17 22:41:49 +0200

    ir/instruction: Add support for alloca, load, store and getelementptr instructions.

--- ir/instruction/memory.go
+++ ir/instruction/memory.go
@@ -1,6 +1,13 @@
 package instruction
 
-import "github.com/llir/llvm/ir/types"
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/llir/llvm/ir/types"
+	"github.com/llir/llvm/ir/value"
+	"github.com/mewkiz/pkg/errutil"
+)
 
 // References:
 //    http://llvm.org/docs/LangRef.html#memory-access-and-addressing-operations
@@ -14,20 +21,98 @@ import "github.com/llir/llvm/ir/types"
 //    http://llvm.org/docs/LangRef.html#atomicrmw-instruction
 //    http://llvm.org/docs/LangRef.html#getelementptr-instruction
 
-type Alloca struct{}
+// Alloca represents an alloca instruction.
+type Alloca struct {
+	// Element type.
+	typ types.Type
+	// Number of elements.
+	nelems int
+}
 
-func (*Alloca) Type() types.Type { panic("Alloca.Type: not yet implemented") }
-func (*Alloca) String() string   { panic("Alloca.String: not yet implemented") }
+// NewAlloca returns a new alloca instruction based on the given element type
+// and number of elments.
+func NewAlloca(typ types.Type, nelems int) (*Alloca, error) {
+	// TODO: Add sanity check for nelems?
+	return &Alloca{typ: typ, nelems: nelems}, nil
+}
 
-type Load struct{}
+// Type returns the type of the value produced by the instruction.
+func (inst *Alloca) Type() types.Type {
+	return inst.typ
+}
 
-func (*Load) Type() types.Type { panic("Load.Type: not yet implemented") }
-func (*Load) String() string   { panic("Load.String: not yet implemented") }
+// String returns the string representation of the instruction.
+func (inst *Alloca) String() string {
+	if inst.nelems != 1 {
+		return fmt.Sprintf("alloca %s, i32 %d", inst.typ, inst.nelems)
+	}
+	return fmt.Sprintf("alloca %s", inst.typ)
+}
 
-type Store struct{}
+// Load represents a load instruction.
+type Load struct {
+	// The type of the value to load from memory.
+	typ types.Type
+	// Memory address from which to load.
+	addr value.Value
+}
 
-func (*Store) Type() types.Type { panic("Store.Type: not yet implemented") }
-func (*Store) String() string   { panic("Store.String: not yet implemented") }
+// NewLoad returns a new load instruction based on the given type and address.
+func NewLoad(typ types.Type, addr value.Value) (*Load, error) {
+	// Sanity checks.
+	switch addrType := addr.Type().(type) {
+	case *types.Pointer:
+		if elem := addrType.Elem(); types.Equal(typ, elem) {
+			return nil, errutil.Newf("type mismatch between %v and %v", typ, elem)
+		}
+	default:
+		return nil, errutil.Newf("invalid pointer type; expected *types.Pointer, got %T", addrType)
+	}
+	return &Load{typ: typ, addr: addr}, nil
+}
+
+// Type returns the type of the value produced by the instruction.
+func (inst *Load) Type() types.Type {
+	return inst.typ
+}
+
+// String returns the string representation of the instruction.
+func (inst *Load) String() string {
+	return fmt.Sprintf("load %s, %s %s", inst.typ, inst.addr.Type(), inst.addr)
+}
+
+// Store represents a store instruction.
+type Store struct {
+	// The value to store in memory.
+	val value.Value
+	// Memory address at which to store.
+	addr value.Value
+}
+
+// NewStore returns a new store instruction based on the given value and
+// address.
+func NewStore(val, addr value.Value) (*Store, error) {
+	// Sanity checks.
+	switch addrType := addr.Type().(type) {
+	case *types.Pointer:
+		if elem := addrType.Elem(); types.Equal(val.Type(), elem) {
+			return nil, errutil.Newf("type mismatch between %v and %v", val.Type(), elem)
+		}
+	default:
+		return nil, errutil.Newf("invalid pointer type; expected *types.Pointer, got %T", addrType)
+	}
+	return &Store{val: val, addr: addr}, nil
+}
+
+// Type returns the type of the value produced by the instruction.
+func (inst *Store) Type() types.Type {
+	return types.NewVoid()
+}
+
+// String returns the string representation of the instruction.
+func (inst *Store) String() string {
+	return fmt.Sprintf("store %s %s, %s %s", inst.val.Type(), inst.val, inst.addr.Type(), inst.addr)
+}
 
 type Fence struct{}
 
@@ -44,10 +129,47 @@ type AtomicRMW struct{}
 func (*AtomicRMW) Type() types.Type { panic("AtomicRMW.Type: not yet implemented") }
 func (*AtomicRMW) String() string   { panic("AtomicRMW.String: not yet implemented") }
 
-type GetElementPtr struct{}
+// GetElementPtr represents a getelementptr instruction.
+type GetElementPtr struct {
+	// Element type.
+	typ types.Type
+	// Memory address of the element.
+	addr value.Value
+	// Element indices.
+	indices []value.Value
+}
 
-func (*GetElementPtr) Type() types.Type { panic("GetElementPtr.Type: not yet implemented") }
-func (*GetElementPtr) String() string   { panic("GetElementPtr.String: not yet implemented") }
+// NewGetElementPtr returns a new getelementptr instruction based on the given
+// type, address and element indices.
+func NewGetElementPtr(typ types.Type, addr value.Value, indices []value.Value) (*GetElementPtr, error) {
+	// Sanity checks.
+	switch addrType := addr.Type().(type) {
+	case *types.Pointer:
+		if elem := addrType.Elem(); types.Equal(typ, elem) {
+			return nil, errutil.Newf("type mismatch between %v and %v", typ, elem)
+		}
+	default:
+		return nil, errutil.Newf("invalid pointer type; expected *types.Pointer, got %T", addrType)
+	}
+	return &GetElementPtr{typ: typ, addr: addr, indices: indices}, nil
+}
+
+// Type returns the type of the value produced by the instruction.
+func (inst *GetElementPtr) Type() types.Type {
+	return inst.typ
+}
+
+// String returns the string representation of the instruction.
+func (inst *GetElementPtr) String() string {
+	if len(inst.indices) > 0 {
+		indicesBuf := new(bytes.Buffer)
+		for _, index := range inst.indices {
+			fmt.Fprintf(indicesBuf, ", %s %s", index.Type(), index)
+		}
+		return fmt.Sprintf("getelementptr %s, %s %s %s", inst.typ, inst.addr.Type(), inst.addr, indicesBuf)
+	}
+	return fmt.Sprintf("getelementptr %s, %s %s", inst.typ, inst.addr.Type(), inst.addr)
+}
 
 // isValueInst ensures that only instructions which return values can be
 // assigned to the Value interface.
