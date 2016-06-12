80c0381fbcc49f7e977a92d5abd03a8ccbaabb96
mewmew <rnd0x00@gmail.com>
2016-06-12 17:16:09 +0200

    ir: Minor update of target specifier comments.

--- ir/module.go
+++ ir/module.go
@@ -17,7 +17,7 @@ type Module struct {
 	// Data layout of the target.
 	//
 	// Examples:
-	//    target datalayout = "e-m:e-i64:64-f80:128-n8:16:32:64-S128"
+	//    "e-m:e-i64:64-f80:128-n8:16:32:64-S128"
 	//
 	// References:
 	//    http://llvm.org/docs/LangRef.html#data-layout
@@ -26,7 +26,7 @@ type Module struct {
 	// vendor of the target.
 	//
 	// Examples:
-	//    x86_64-unknown-linux-gnu
+	//    "x86_64-unknown-linux-gnu"
 	//
 	// References:
 	//    http://llvm.org/docs/LangRef.html#target-triple
