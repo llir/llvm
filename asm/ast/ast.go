b217ce8c59f465f4abb25ebb81ccb8505693b9f4
mewmew <rnd0x00@gmail.com>
2016-08-24 14:26:09 +0200

    ast: Remove linkage for now, to keep the ast package minimal.
    The intention is to keep the AST package minimal for now,
    and support only the LLVM IR assembly directives currently
    defined by the llir/llvm/ir package.
    
    Updates #15.

--- asm/ast/ast.go
+++ asm/ast/ast.go
@@ -2,8 +2,6 @@
 // LLVM IR assembly files.
 package ast
 
-import "fmt"
-
 // === [ Modules ] =============================================================
 
 // A Module represents an LLVM IR module.
@@ -88,8 +86,6 @@ type TypeDef struct {
 type GlobalVarDecl struct {
 	// Global variable name.
 	Name string
-	// Linkage type of the global variable.
-	Linkage Linkage
 	// The address of the global variable is not significant, only its content.
 	UnnamedAddr bool
 	// Immutability of the global variable.
@@ -100,62 +96,6 @@ type GlobalVarDecl struct {
 	Val Value
 	// Alignment; or 0 if not aligned.
 	Align int
-}
-
-// Linkage specifies the linkage type of a global variable or function.
-//
-// Examples:
-//    linkonce_odr
-//
-// References:
-//    http://llvm.org/docs/LangRef.html#linkage
-type Linkage int
-
-// Linkage types.
-const (
-	LinkageNone                Linkage = iota
-	LinkageAppending                   // Valid for global variable definition.
-	LinkageAvailableExternally         // Valid for global variable definition or function definition.
-	LinkageCommon                      // Valid for global variable definition.
-	LinkageExternal                    // Valid for external global variable declaration, function declaration or function defintion.
-	LinkageExternWeak                  // Valid for external global variable declaration or function declaration.
-	LinkageInternal                    // Valid for global variable definition or function definition.
-	LinkageLinkonce                    // Valid for global variable definition or function definition.
-	LinkageLinkonceODR                 // Valid for global variable definition or function definition.
-	LinkagePrivate                     // Valid for global variable definition or function definition.
-	LinkageWeak                        // Valid for global variable definition or function definition.
-	LinkageWeakODR                     // Valid for global variable definition or function definition.
-)
-
-// String returns the string representation of the linkage type.
-func (l Linkage) String() string {
-	switch l {
-	case LinkageNone:
-		return "none"
-	case LinkageAppending:
-		return "appending"
-	case LinkageAvailableExternally:
-		return "available_externally"
-	case LinkageCommon:
-		return "common"
-	case LinkageExternal:
-		return "external"
-	case LinkageExternWeak:
-		return "extern_weak"
-	case LinkageInternal:
-		return "internal"
-	case LinkageLinkonce:
-		return "linkonce"
-	case LinkageLinkonceODR:
-		return "linkonce_odr"
-	case LinkagePrivate:
-		return "private"
-	case LinkageWeak:
-		return "weak"
-	case LinkageWeakODR:
-		return "weak_odr"
-	}
-	panic(fmt.Sprintf("support for linkage type %d not yet implemented", int(l)))
 }
 
 // --- [ Function declarations ] -----------------------------------------------
@@ -174,8 +114,6 @@ func (l Linkage) String() string {
 // References:
 //    http://llvm.org/docs/LangRef.html#functions
 type FuncDecl struct {
-	// Linkage type of the function declaration.
-	Linkage Linkage
 	// Function name.
 	Name string
 	// Function signature.
