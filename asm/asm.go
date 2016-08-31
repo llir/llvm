c296aeb179d3a62f256b73223d5e1b3a284aae49
mewmew <rnd0x00@gmail.com>
2016-08-31 12:57:15 +0200

    ir/internal/enc: Move EncGlobal and EncLocal from asm package to internal enc package.

--- asm/asm.go
+++ asm/asm.go
@@ -2,128 +2,56 @@
 // representation of LLVM IR.
 package asm
 
-import "strings"
+import (
+	"go/ast"
+	"io"
+	"io/ioutil"
 
-// EncGlobal encodes a global name to its LLVM IR assembly representation.
-//
-// Examples:
-//    "foo" -> "@foo"
-//    "a b" -> `@"a\20b"`
-//    "世" -> `@"\E4\B8\96"`
-//
-// References:
-//    http://www.llvm.org/docs/LangRef.html#identifiers
-func EncGlobal(name string) string {
-	return "@" + Escape(name)
-}
-
-// EncLocal encodes a local name to its LLVM IR assembly representation.
-//
-// Examples:
-//    "foo" -> "%foo"
-//    "a b" -> `%"a\20b"`
-//    "世" -> `%"\E4\B8\96"`
-//
-// References:
-//    http://www.llvm.org/docs/LangRef.html#identifiers
-func EncLocal(name string) string {
-	return "%" + Escape(name)
-}
-
-const (
-	// decimal specifies the decimal digit characters.
-	decimal = "0123456789"
-	// upper specifies the uppercase letters.
-	upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	// lower specifies the lowercase letters.
-	lower = "abcdefghijklmnopqrstuvwxyz"
-	// alpha specifies the alphabetic characters.
-	alpha = upper + lower
-	// head is the set of valid characters for the first character of an
-	// identifier.
-	head = alpha + "$-._"
-	// tail is the set of valid characters for the remaining characters of an
-	// identifier (i.e. all characters in the identifier except the first). All
-	// characters of a label may be from the tail set, even the first character.
-	tail = head + decimal
+	"github.com/llir/llvm/asm/internal/lexer"
+	"github.com/llir/llvm/asm/internal/parser"
+	"github.com/mewkiz/pkg/errutil"
 )
 
-// Escape replaces any characters which are not valid in identifiers with
-// corresponding hexadecimal escape sequence (\XX).
-func Escape(s string) string {
-	// Check if a replacement is required.
-	extra := 0
-	for i := 0; i < len(s); i++ {
-		if strings.IndexByte(tail, s[i]) == -1 {
-			// Two extra bytes are required for each invalid byte; e.g.
-			//    "#" -> `\23`
-			//    "世" -> `\E4\B8\96`
-			extra += 2
-		}
+// ParseFile parses the given LLVM IR assembly file into an LLVM IR module.
+func ParseFile(path string) (*ast.File, error) {
+	buf, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, errutil.Err(err)
 	}
-	if extra == 0 {
-		return s
-	}
-
-	// Replace invalid characters.
-	const hextable = "0123456789ABCDEF"
-	buf := make([]byte, len(s)+extra)
-	j := 0
-	for i := 0; i < len(s); i++ {
-		b := s[i]
-		if strings.IndexByte(tail, b) != -1 {
-			buf[j] = b
-			j++
-			continue
-		}
-		buf[j] = '\\'
-		buf[j+1] = hextable[b>>4]
-		buf[j+2] = hextable[b&0x0F]
-		j += 3
-	}
-	return string(buf)
+	return ParseBytes(buf)
 }
 
-// Unescape replaces hexadecimal escape sequences (\xx) in s with their
-// corresponding characters.
-func Unescape(s string) string {
-	if !strings.ContainsRune(s, '\\') {
-		return s
+// Parse parses the given LLVM IR assembly file into an LLVM IR module, reading
+// from r.
+func Parse(r io.Reader) (*ast.File, error) {
+	buf, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, errutil.Err(err)
 	}
-	j := 0
-	buf := []byte(s)
-	for i := 0; i < len(s); i++ {
-		b := s[i]
-		if b == '\\' && i+2 < len(s) {
-			x1, ok := unhex(s[i+1])
-			if ok {
-				x2, ok := unhex(s[i+2])
-				if ok {
-					b = x1<<4 | x2
-					i += 2
-				}
-			}
-		}
-		if i != j {
-			buf[j] = b
-		}
-		j++
-	}
-	return string(buf[:j])
+	return ParseBytes(buf)
 }
 
-// unhex returns the numeric value represented by the hexadecimal digit b. It
-// returns false if b is not a hexadecimal digit.
-func unhex(b byte) (v byte, ok bool) {
-	// This is an adapted copy of the unhex function from the strconv package,
-	// which is goverend by a BSD-style license.
-	switch {
-	case '0' <= b && b <= '9':
-		return b - '0', true
-	case 'a' <= b && b <= 'f':
-		return b - 'a' + 10, true
-	case 'A' <= b && b <= 'F':
-		return b - 'A' + 10, true
+// ParseBytes parses the given LLVM IR assembly file into an LLVM IR module,
+// reading from b.
+func ParseBytes(b []byte) (*ast.File, error) {
+	l := lexer.NewLexer(b)
+	p := parser.NewParser()
+	file, err := p.Parse(l)
+	if err != nil {
+		return nil, errutil.Err(err)
 	}
-	return 0, false
+	f, ok := file.(*ast.File)
+	if !ok {
+		return nil, errutil.Newf("invalid file type; expected *ast.File, got %T", file)
+	}
+	if err := check(f); err != nil {
+		return nil, errutil.Err(err)
+	}
+	return f, nil
+}
+
+// ParseString parses the given LLVM IR assembly file into an LLVM IR module,
+// reading from s.
+func ParseString(s string) (*ast.File, error) {
+	return ParseBytes([]byte(s))
 }
--- ir/internal/enc/enc_test.go
+++ ir/internal/enc/enc_test.go
@@ -1,12 +1,12 @@
-package asm_test
+package enc_test
 
 import (
 	"testing"
 
-	"github.com/llir/llvm/asm"
+	"github.com/llir/llvm/internal/enc"
 )
 
-func TestEncGlobal(t *testing.T) {
+func TestGlobal(t *testing.T) {
 	golden := []struct {
 		s    string
 		want string
@@ -34,14 +34,14 @@ func TestEncGlobal(t *testing.T) {
 	}
 
 	for i, g := range golden {
-		got := asm.EncGlobal(g.s)
+		got := enc.Global(g.s)
 		if got != g.want {
 			t.Errorf("i=%d: name mismatch; expected %q, got %q", i, g.want, got)
 		}
 	}
 }
 
-func TestEncLocal(t *testing.T) {
+func TestLocal(t *testing.T) {
 	golden := []struct {
 		s    string
 		want string
@@ -69,33 +69,33 @@ func TestEncLocal(t *testing.T) {
 	}
 
 	for i, g := range golden {
-		got := asm.EncLocal(g.s)
+		got := enc.Local(g.s)
 		if got != g.want {
 			t.Errorf("i=%d: name mismatch; expected %q, got %q", i, g.want, got)
 		}
 	}
 }
 
-func BenchmarkEncGlobalNoReplace(b *testing.B) {
+func BenchmarkGlobalNoReplace(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		asm.EncGlobal("$foo_bar_baz")
+		enc.Global("$foo_bar_baz")
 	}
 }
 
-func BenchmarkEncGlobalReplace(b *testing.B) {
+func BenchmarkGlobalReplace(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		asm.EncGlobal("$foo bar#baz")
+		enc.Global("$foo bar#baz")
 	}
 }
 
-func BenchmarkEncLocalNoReplace(b *testing.B) {
+func BenchmarkLocalNoReplace(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		asm.EncLocal("$foo_bar_baz")
+		enc.Local("$foo_bar_baz")
 	}
 }
 
-func BenchmarkEncLocalReplace(b *testing.B) {
+func BenchmarkLocalReplace(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		asm.EncLocal("$foo bar#baz")
+		enc.Local("$foo bar#baz")
 	}
 }
