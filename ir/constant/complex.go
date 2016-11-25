4f24caed96f3c5874414f139b74828e3aa1766b3
mewmew <rnd0x00@gmail.com>
2016-11-25 15:02:09 +0100

    ir: Add support for vector, array and struct constants.

--- ir/constant/complex.go
+++ ir/constant/complex.go
@@ -5,21 +5,149 @@
 
 package constant
 
-import "github.com/llir/llvm/ir/types"
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/llir/llvm/ir/types"
+)
 
 // --- [ vector ] --------------------------------------------------------------
 
+// Vector represents a vector constant.
 type Vector struct {
+	// Vector elements.
+	elems []Constant
+	// Vector type.
+	typ *types.VectorType
+}
+
+// NewVector returns a new vector constant based on the given elements.
+func NewVector(elems ...Constant) *Vector {
+	if len(elems) < 1 {
+		panic(fmt.Sprintf("invalid number of vector elements; expected > 0, got %d", len(elems)))
+	}
+	typ := types.NewVector(elems[0].Type(), len(elems))
+	return &Vector{elems: elems, typ: typ}
+}
+
+// Type returns the type of the constant.
+func (c *Vector) Type() types.Type {
+	return c.typ
+}
+
+// Ident returns the value of the constant.
+func (c *Vector) Ident() string {
+	buf := &bytes.Buffer{}
+	buf.WriteString("<")
+	for i, elem := range c.Elems() {
+		if i != 0 {
+			buf.WriteString(", ")
+		}
+		fmt.Fprintf(buf, "%s %s",
+			elem.Type(),
+			elem.Ident())
+	}
+	buf.WriteString(">")
+	return buf.String()
+}
+
+// Elems returns the elements of the vector constant.
+func (c *Vector) Elems() []Constant {
+	return c.elems
 }
 
 // --- [ array ] ---------------------------------------------------------------
 
+// TODO: Add support for character arrays.
+
+// Array represents an array constant.
 type Array struct {
+	// Array elements.
+	elems []Constant
+	// Array type.
+	typ *types.ArrayType
+}
+
+// NewArray returns a new array constant based on the given elements.
+func NewArray(elems ...Constant) *Array {
+	if len(elems) < 1 {
+		panic(fmt.Sprintf("invalid number of array elements; expected > 0, got %d", len(elems)))
+	}
+	typ := types.NewArray(elems[0].Type(), len(elems))
+	return &Array{elems: elems, typ: typ}
+}
+
+// Type returns the type of the constant.
+func (c *Array) Type() types.Type {
+	return c.typ
+}
+
+// Ident returns the value of the constant.
+func (c *Array) Ident() string {
+	buf := &bytes.Buffer{}
+	buf.WriteString("[")
+	for i, elem := range c.Elems() {
+		if i != 0 {
+			buf.WriteString(", ")
+		}
+		fmt.Fprintf(buf, "%s %s",
+			elem.Type(),
+			elem.Ident())
+	}
+	buf.WriteString("]")
+	return buf.String()
+}
+
+// Elems returns the elements of the array constant.
+func (c *Array) Elems() []Constant {
+	return c.elems
 }
 
 // --- [ struct ] --------------------------------------------------------------
 
+// Struct represents a struct constant.
 type Struct struct {
+	// Struct fields.
+	fields []Constant
+	// Struct type.
+	typ *types.StructType
+}
+
+// NewStruct returns a new struct constant based on the given struct fields.
+func NewStruct(fields ...Constant) *Struct {
+	var fieldTypes []types.Type
+	for _, field := range fields {
+		fieldTypes = append(fieldTypes, field.Type())
+	}
+	typ := types.NewStruct(fieldTypes...)
+	return &Struct{fields: fields, typ: typ}
+}
+
+// Type returns the type of the constant.
+func (c *Struct) Type() types.Type {
+	return c.typ
+}
+
+// Ident returns the value of the constant.
+func (c *Struct) Ident() string {
+	buf := &bytes.Buffer{}
+	buf.WriteString("{")
+	for i, field := range c.Fields() {
+		if i != 0 {
+			buf.WriteString(", ")
+		}
+		fmt.Fprintf(buf, "%s %s",
+			field.Type(),
+			field.Ident())
+	}
+	buf.WriteString("}")
+	return buf.String()
+}
+
+// Fields returns the struct fields of the struct constant.
+func (c *Struct) Fields() []Constant {
+	return c.fields
 }
 
 // --- [ zeroinitializer ] -----------------------------------------------------
--- ir/constant/constant_test.go
+++ ir/constant/constant_test.go
@@ -6,6 +6,14 @@ import (
 
 // Valutate that the relevant types satisfy the constant.Constant interface.
 var (
+	// Simple constants.
 	_ constant.Constant = &constant.Int{}
 	_ constant.Constant = &constant.Float{}
+	_ constant.Constant = &constant.Null{}
+	// Complex constants.
+	_ constant.Constant = &constant.Vector{}
+	_ constant.Constant = &constant.Array{}
+	_ constant.Constant = &constant.Struct{}
+	_ constant.Constant = &constant.ZeroInitializer{}
+	// Constant expressions.
 )
