db585e2f8215724c7c5bbf7f9ba670d8aa6f8fe7
mewmew <rnd0x00@gmail.com>
2016-11-26 21:40:48 +0100

    ir/types: Extend test cases.

--- ir/types/types_test.go
+++ ir/types/types_test.go
@@ -153,6 +153,376 @@ func TestStructTypeString(t *testing.T) {
 	}
 }
 
+func TestIsVoid(t *testing.T) {
+	golden := []struct {
+		want bool
+		typ  types.Type
+	}{
+		{want: true, typ: types.Void},
+		{want: true, typ: &types.VoidType{}},
+		{want: false, typ: types.Label},
+		{want: false, typ: &types.LabelType{}},
+		{want: false, typ: &types.IntType{}},
+		{want: false, typ: types.I1},
+		{want: false, typ: types.I8},
+		{want: false, typ: types.I16},
+		{want: false, typ: types.I32},
+		{want: false, typ: types.I64},
+		{want: false, typ: types.I128},
+		{want: false, typ: &types.FloatType{}},
+		{want: false, typ: types.Half},
+		{want: false, typ: types.Float},
+		{want: false, typ: types.Double},
+		{want: false, typ: types.FP128},
+		{want: false, typ: types.X86_FP80},
+		{want: false, typ: types.PPC_FP128},
+		{want: false, typ: &types.FuncType{}},
+		{want: false, typ: &types.PointerType{}},
+		{want: false, typ: &types.VectorType{}},
+		{want: false, typ: &types.ArrayType{}},
+		{want: false, typ: &types.StructType{}},
+	}
+	for i, g := range golden {
+		got := types.IsVoid(g.typ)
+		if got != g.want {
+			t.Errorf("i=%d; expected %v, got %v", i, g.want, got)
+		}
+	}
+}
+
+func TestIsLabel(t *testing.T) {
+	golden := []struct {
+		want bool
+		typ  types.Type
+	}{
+		{want: false, typ: types.Void},
+		{want: false, typ: &types.VoidType{}},
+		{want: true, typ: types.Label},
+		{want: true, typ: &types.LabelType{}},
+		{want: false, typ: &types.IntType{}},
+		{want: false, typ: types.I1},
+		{want: false, typ: types.I8},
+		{want: false, typ: types.I16},
+		{want: false, typ: types.I32},
+		{want: false, typ: types.I64},
+		{want: false, typ: types.I128},
+		{want: false, typ: &types.FloatType{}},
+		{want: false, typ: types.Half},
+		{want: false, typ: types.Float},
+		{want: false, typ: types.Double},
+		{want: false, typ: types.FP128},
+		{want: false, typ: types.X86_FP80},
+		{want: false, typ: types.PPC_FP128},
+		{want: false, typ: &types.FuncType{}},
+		{want: false, typ: &types.PointerType{}},
+		{want: false, typ: &types.VectorType{}},
+		{want: false, typ: &types.ArrayType{}},
+		{want: false, typ: &types.StructType{}},
+	}
+	for i, g := range golden {
+		got := types.IsLabel(g.typ)
+		if got != g.want {
+			t.Errorf("i=%d; expected %v, got %v", i, g.want, got)
+		}
+	}
+}
+
+func TestIsBool(t *testing.T) {
+	golden := []struct {
+		want bool
+		typ  types.Type
+	}{
+		{want: false, typ: types.Void},
+		{want: false, typ: &types.VoidType{}},
+		{want: false, typ: types.Label},
+		{want: false, typ: &types.LabelType{}},
+		{want: false, typ: &types.IntType{}},
+		{want: true, typ: types.I1},
+		{want: false, typ: types.I8},
+		{want: false, typ: types.I16},
+		{want: false, typ: types.I32},
+		{want: false, typ: types.I64},
+		{want: false, typ: types.I128},
+		{want: false, typ: &types.FloatType{}},
+		{want: false, typ: types.Half},
+		{want: false, typ: types.Float},
+		{want: false, typ: types.Double},
+		{want: false, typ: types.FP128},
+		{want: false, typ: types.X86_FP80},
+		{want: false, typ: types.PPC_FP128},
+		{want: false, typ: &types.FuncType{}},
+		{want: false, typ: &types.PointerType{}},
+		{want: false, typ: &types.VectorType{}},
+		{want: false, typ: &types.ArrayType{}},
+		{want: false, typ: &types.StructType{}},
+	}
+	for i, g := range golden {
+		got := types.IsBool(g.typ)
+		if got != g.want {
+			t.Errorf("i=%d; expected %v, got %v", i, g.want, got)
+		}
+	}
+}
+
+func TestIsInt(t *testing.T) {
+	golden := []struct {
+		want bool
+		typ  types.Type
+	}{
+		{want: false, typ: types.Void},
+		{want: false, typ: &types.VoidType{}},
+		{want: false, typ: types.Label},
+		{want: false, typ: &types.LabelType{}},
+		{want: true, typ: &types.IntType{}},
+		{want: true, typ: types.I1},
+		{want: true, typ: types.I8},
+		{want: true, typ: types.I16},
+		{want: true, typ: types.I32},
+		{want: true, typ: types.I64},
+		{want: true, typ: types.I128},
+		{want: false, typ: &types.FloatType{}},
+		{want: false, typ: types.Half},
+		{want: false, typ: types.Float},
+		{want: false, typ: types.Double},
+		{want: false, typ: types.FP128},
+		{want: false, typ: types.X86_FP80},
+		{want: false, typ: types.PPC_FP128},
+		{want: false, typ: &types.FuncType{}},
+		{want: false, typ: &types.PointerType{}},
+		{want: false, typ: &types.VectorType{}},
+		{want: false, typ: &types.ArrayType{}},
+		{want: false, typ: &types.StructType{}},
+	}
+	for i, g := range golden {
+		got := types.IsInt(g.typ)
+		if got != g.want {
+			t.Errorf("i=%d; expected %v, got %v", i, g.want, got)
+		}
+	}
+}
+
+func TestIsFloat(t *testing.T) {
+	golden := []struct {
+		want bool
+		typ  types.Type
+	}{
+		{want: false, typ: types.Void},
+		{want: false, typ: &types.VoidType{}},
+		{want: false, typ: types.Label},
+		{want: false, typ: &types.LabelType{}},
+		{want: false, typ: &types.IntType{}},
+		{want: false, typ: types.I1},
+		{want: false, typ: types.I8},
+		{want: false, typ: types.I16},
+		{want: false, typ: types.I32},
+		{want: false, typ: types.I64},
+		{want: false, typ: types.I128},
+		{want: true, typ: &types.FloatType{}},
+		{want: true, typ: types.Half},
+		{want: true, typ: types.Float},
+		{want: true, typ: types.Double},
+		{want: true, typ: types.FP128},
+		{want: true, typ: types.X86_FP80},
+		{want: true, typ: types.PPC_FP128},
+		{want: false, typ: &types.FuncType{}},
+		{want: false, typ: &types.PointerType{}},
+		{want: false, typ: &types.VectorType{}},
+		{want: false, typ: &types.ArrayType{}},
+		{want: false, typ: &types.StructType{}},
+	}
+	for i, g := range golden {
+		got := types.IsFloat(g.typ)
+		if got != g.want {
+			t.Errorf("i=%d; expected %v, got %v", i, g.want, got)
+		}
+	}
+}
+
+func TestIsFunc(t *testing.T) {
+	golden := []struct {
+		want bool
+		typ  types.Type
+	}{
+		{want: false, typ: types.Void},
+		{want: false, typ: &types.VoidType{}},
+		{want: false, typ: types.Label},
+		{want: false, typ: &types.LabelType{}},
+		{want: false, typ: &types.IntType{}},
+		{want: false, typ: types.I1},
+		{want: false, typ: types.I8},
+		{want: false, typ: types.I16},
+		{want: false, typ: types.I32},
+		{want: false, typ: types.I64},
+		{want: false, typ: types.I128},
+		{want: false, typ: &types.FloatType{}},
+		{want: false, typ: types.Half},
+		{want: false, typ: types.Float},
+		{want: false, typ: types.Double},
+		{want: false, typ: types.FP128},
+		{want: false, typ: types.X86_FP80},
+		{want: false, typ: types.PPC_FP128},
+		{want: true, typ: &types.FuncType{}},
+		{want: false, typ: &types.PointerType{}},
+		{want: false, typ: &types.VectorType{}},
+		{want: false, typ: &types.ArrayType{}},
+		{want: false, typ: &types.StructType{}},
+	}
+	for i, g := range golden {
+		got := types.IsFunc(g.typ)
+		if got != g.want {
+			t.Errorf("i=%d; expected %v, got %v", i, g.want, got)
+		}
+	}
+}
+
+func TestIsPointer(t *testing.T) {
+	golden := []struct {
+		want bool
+		typ  types.Type
+	}{
+		{want: false, typ: types.Void},
+		{want: false, typ: &types.VoidType{}},
+		{want: false, typ: types.Label},
+		{want: false, typ: &types.LabelType{}},
+		{want: false, typ: &types.IntType{}},
+		{want: false, typ: types.I1},
+		{want: false, typ: types.I8},
+		{want: false, typ: types.I16},
+		{want: false, typ: types.I32},
+		{want: false, typ: types.I64},
+		{want: false, typ: types.I128},
+		{want: false, typ: &types.FloatType{}},
+		{want: false, typ: types.Half},
+		{want: false, typ: types.Float},
+		{want: false, typ: types.Double},
+		{want: false, typ: types.FP128},
+		{want: false, typ: types.X86_FP80},
+		{want: false, typ: types.PPC_FP128},
+		{want: false, typ: &types.FuncType{}},
+		{want: true, typ: &types.PointerType{}},
+		{want: false, typ: &types.VectorType{}},
+		{want: false, typ: &types.ArrayType{}},
+		{want: false, typ: &types.StructType{}},
+	}
+	for i, g := range golden {
+		got := types.IsPointer(g.typ)
+		if got != g.want {
+			t.Errorf("i=%d; expected %v, got %v", i, g.want, got)
+		}
+	}
+}
+
+func TestIsVector(t *testing.T) {
+	golden := []struct {
+		want bool
+		typ  types.Type
+	}{
+		{want: false, typ: types.Void},
+		{want: false, typ: &types.VoidType{}},
+		{want: false, typ: types.Label},
+		{want: false, typ: &types.LabelType{}},
+		{want: false, typ: &types.IntType{}},
+		{want: false, typ: types.I1},
+		{want: false, typ: types.I8},
+		{want: false, typ: types.I16},
+		{want: false, typ: types.I32},
+		{want: false, typ: types.I64},
+		{want: false, typ: types.I128},
+		{want: false, typ: &types.FloatType{}},
+		{want: false, typ: types.Half},
+		{want: false, typ: types.Float},
+		{want: false, typ: types.Double},
+		{want: false, typ: types.FP128},
+		{want: false, typ: types.X86_FP80},
+		{want: false, typ: types.PPC_FP128},
+		{want: false, typ: &types.FuncType{}},
+		{want: false, typ: &types.PointerType{}},
+		{want: true, typ: &types.VectorType{}},
+		{want: false, typ: &types.ArrayType{}},
+		{want: false, typ: &types.StructType{}},
+	}
+	for i, g := range golden {
+		got := types.IsVector(g.typ)
+		if got != g.want {
+			t.Errorf("i=%d; expected %v, got %v", i, g.want, got)
+		}
+	}
+}
+
+func TestIsArray(t *testing.T) {
+	golden := []struct {
+		want bool
+		typ  types.Type
+	}{
+		{want: false, typ: types.Void},
+		{want: false, typ: &types.VoidType{}},
+		{want: false, typ: types.Label},
+		{want: false, typ: &types.LabelType{}},
+		{want: false, typ: &types.IntType{}},
+		{want: false, typ: types.I1},
+		{want: false, typ: types.I8},
+		{want: false, typ: types.I16},
+		{want: false, typ: types.I32},
+		{want: false, typ: types.I64},
+		{want: false, typ: types.I128},
+		{want: false, typ: &types.FloatType{}},
+		{want: false, typ: types.Half},
+		{want: false, typ: types.Float},
+		{want: false, typ: types.Double},
+		{want: false, typ: types.FP128},
+		{want: false, typ: types.X86_FP80},
+		{want: false, typ: types.PPC_FP128},
+		{want: false, typ: &types.FuncType{}},
+		{want: false, typ: &types.PointerType{}},
+		{want: false, typ: &types.VectorType{}},
+		{want: true, typ: &types.ArrayType{}},
+		{want: false, typ: &types.StructType{}},
+	}
+	for i, g := range golden {
+		got := types.IsArray(g.typ)
+		if got != g.want {
+			t.Errorf("i=%d; expected %v, got %v", i, g.want, got)
+		}
+	}
+}
+
+func TestIsStruct(t *testing.T) {
+	golden := []struct {
+		want bool
+		typ  types.Type
+	}{
+		{want: false, typ: types.Void},
+		{want: false, typ: &types.VoidType{}},
+		{want: false, typ: types.Label},
+		{want: false, typ: &types.LabelType{}},
+		{want: false, typ: &types.IntType{}},
+		{want: false, typ: types.I1},
+		{want: false, typ: types.I8},
+		{want: false, typ: types.I16},
+		{want: false, typ: types.I32},
+		{want: false, typ: types.I64},
+		{want: false, typ: types.I128},
+		{want: false, typ: &types.FloatType{}},
+		{want: false, typ: types.Half},
+		{want: false, typ: types.Float},
+		{want: false, typ: types.Double},
+		{want: false, typ: types.FP128},
+		{want: false, typ: types.X86_FP80},
+		{want: false, typ: types.PPC_FP128},
+		{want: false, typ: &types.FuncType{}},
+		{want: false, typ: &types.PointerType{}},
+		{want: false, typ: &types.VectorType{}},
+		{want: false, typ: &types.ArrayType{}},
+		{want: true, typ: &types.StructType{}},
+	}
+	for i, g := range golden {
+		got := types.IsStruct(g.typ)
+		if got != g.want {
+			t.Errorf("i=%d; expected %v, got %v", i, g.want, got)
+		}
+	}
+}
+
 // Valutate that the relevant types satisfy the value.Value interface.
 var (
 	_ value.Value = &types.Param{}
