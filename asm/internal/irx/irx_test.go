9aff76fc734643c3a8df2300dfae2078a422c207
mewmew <rnd0x00@gmail.com>
2016-11-28 23:24:21 +0100

    irx: Add test case to verify that the fixer has replaced all dummy values.

---
+++ asm/internal/irx/irx_test.go
@@ -0,0 +1,37 @@
+package irx_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/kr/pretty"
+	"github.com/llir/llvm/asm"
+)
+
+func TestFix(t *testing.T) {
+	// Verify that the fixer has replaced all dummy values.
+	golden := []struct {
+		path string
+	}{
+		{path: "../testdata/rand.ll"},
+	}
+	for i, g := range golden {
+		m, err := asm.ParseFile(g.path)
+		if err != nil {
+			t.Errorf("i=%d: unable to parse file; %v", i, err)
+			continue
+		}
+		// Hack :) Rather than using reflect, pretty-print the module and search
+		// for occurances of globalDummy, localDummy and instCallDummy.
+		s := pretty.Sprint(m)
+		if strings.Contains(s, "globalDummy") {
+			t.Errorf("i=%d: module contains dummy global identifiers; `%v`", i, s)
+		}
+		if strings.Contains(s, "localDummy") {
+			t.Errorf("i=%d: module contains dummy local identifiers; `%v`", i, s)
+		}
+		if strings.Contains(s, "instCallDummy") {
+			t.Errorf("i=%d: module contains dummy call instructions; `%v`", i, s)
+		}
+	}
+}
