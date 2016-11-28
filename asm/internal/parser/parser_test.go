2753d30768164526ad42bc1ff7c33171c4ad3223
mewmew <rnd0x00@gmail.com>
2016-11-28 23:15:35 +0100

    parser: Add initial test case.

---
+++ asm/internal/parser/parser_test.go
@@ -0,0 +1,34 @@
+package parser_test
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/llir/llvm/asm"
+)
+
+func TestParseFile(t *testing.T) {
+	// Round-trip test of the parser.
+	golden := []struct {
+		path string
+	}{
+		{path: "../testdata/rand.ll"},
+	}
+	for i, g := range golden {
+		buf, err := ioutil.ReadFile(g.path)
+		if err != nil {
+			t.Errorf("i=%d: unable to read file; %v", i, err)
+			continue
+		}
+		want := string(buf)
+		m, err := asm.ParseString(want)
+		if err != nil {
+			t.Errorf("i=%d: unable to parse file; %v", i, err)
+			continue
+		}
+		got := m.String()
+		if want != got {
+			t.Errorf("i=%d: module mismatch; expected `%v`, got `%v`", i, want, got)
+		}
+	}
+}
