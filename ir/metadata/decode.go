c5acecfd4c975fc43da971af6115f45d9f5f97b2
mewmew <rnd0x00@gmail.com>
2017-04-12 14:38:41 +0200

    ir/metadata: Add preliminary support for decoding metadata strings.

--- ir/metadata/decode.go
+++ ir/metadata/decode.go
@@ -1,3 +1,6 @@
+// Note, the LLVM IR metadata decoder implementation of this package is heavily
+// inspired by encoding/json; which is governed by a BSD license.
+
 package metadata
 
 import (
@@ -40,13 +43,74 @@ func (d *decoder) unmarshal(node Node, v interface{}) error {
 
 // value decodes a metadata node into the value.
 func (d *decoder) value(node Node, v reflect.Value) error {
-	if u, ok := v.Interface().(Unmarshaler); ok {
+	u, rv := d.indirect(v)
+	if u != nil {
 		if err := u.UnmarshalMetadata(node); err != nil {
 			return errors.WithStack(err)
 		}
 		return nil
 	}
-	panic(fmt.Errorf("support for decoding into %v not yet implemented", v.Type()))
+	switch kind := rv.Type().Kind(); kind {
+	case reflect.String:
+		if err := d.string(node, rv); err != nil {
+			return errors.WithStack(err)
+		}
+		return nil
+	default:
+		panic(fmt.Errorf("support for decoding into %T (%v) not yet implemented", kind, kind))
+	}
+}
+
+func (d *decoder) string(node Node, v reflect.Value) error {
+	n, ok := node.(*Metadata)
+	if !ok {
+		return errors.Errorf("invalid metadata node type; expected *metadata.Metadata, got %T", node)
+	}
+	if len(n.Nodes) != 1 {
+		return errors.Errorf("invalid number of metadata nodes; expected 1, got %d", len(n.Nodes))
+	}
+	s, ok := n.Nodes[0].(*String)
+	if !ok {
+		return errors.Errorf("invalid metadata string type; expected *metadata.String, got %T", n.Nodes[0])
+	}
+	v.SetString(s.Val)
+	return nil
+}
+
+// indirect walks down v allocating pointers as needed, until it gets to a non-
+// pointer. if it encounters an Unmarshaler, indirect stops and returns that.
+func (d *decoder) indirect(v reflect.Value) (Unmarshaler, reflect.Value) {
+	// If v is a named type and is addressable, start with its address, so that
+	// if the type has pointer methods, we find them.
+	if v.Kind() != reflect.Ptr && v.Type().Name() != "" && v.CanAddr() {
+		v = v.Addr()
+	}
+	for {
+		// Load value from interface, but only if the result will be usefully
+		// addressable.
+		if v.Kind() == reflect.Interface && !v.IsNil() {
+			e := v.Elem()
+			if e.Kind() == reflect.Ptr && !e.IsNil() && e.Elem().Kind() == reflect.Ptr {
+				v = e
+				continue
+			}
+		}
+
+		if v.Kind() != reflect.Ptr {
+			break
+		}
+
+		if v.IsNil() {
+			v.Set(reflect.New(v.Type().Elem()))
+		}
+		if v.Type().NumMethod() > 0 {
+			if u, ok := v.Interface().(Unmarshaler); ok {
+				return u, reflect.Value{}
+			}
+		}
+		v = v.Elem()
+	}
+	return nil, v
 }
 
 // Unmarshaler is the interface implemented by types that can unmarshal an LLVM
