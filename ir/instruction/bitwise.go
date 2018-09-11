9a71deaeb656072e18df3d15a0e3fc33cdb30164
Robin Eklind <rnd0x00@gmail.com>
2018-09-11 11:23:25 +0200

    ir/instruction: add scaffolding for terminators

--- ir/instruction/bitwise.go
+++ ir/instruction/bitwise.go
@@ -1,39 +1,95 @@
 package instruction
 
+import "github.com/llir/l/ir/value"
+
 // --- [ Bitwise instructions ] ------------------------------------------------
 
 // ~~~ [ shl ] ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // Shl is an LLVM IR shl instruction.
 type Shl struct {
+	// Name of local variable associated with the result.
+	LocalName string
+	// Operands.
+	X, Y value.Value
+}
+
+// NewShl returns a new shl instruction based on the given operands.
+func NewShl(x, y value.Value) *Shl {
+	return &Shl{X: x, Y: y}
 }
 
 // ~~~ [ lshr ] ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // LShr is an LLVM IR lshr instruction.
 type LShr struct {
+	// Name of local variable associated with the result.
+	LocalName string
+	// Operands.
+	X, Y value.Value
+}
+
+// NewLShr returns a new lshr instruction based on the given operands.
+func NewLShr(x, y value.Value) *LShr {
+	return &LShr{X: x, Y: y}
 }
 
 // ~~~ [ ashr ] ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // AShr is an LLVM IR ashr instruction.
 type AShr struct {
+	// Name of local variable associated with the result.
+	LocalName string
+	// Operands.
+	X, Y value.Value
+}
+
+// NewAShr returns a new ashr instruction based on the given operands.
+func NewAShr(x, y value.Value) *AShr {
+	return &AShr{X: x, Y: y}
 }
 
 // ~~~ [ and ] ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // And is an LLVM IR and instruction.
 type And struct {
+	// Name of local variable associated with the result.
+	LocalName string
+	// Operands.
+	X, Y value.Value
+}
+
+// NewAnd returns a new and instruction based on the given operands.
+func NewAnd(x, y value.Value) *And {
+	return &And{X: x, Y: y}
 }
 
 // ~~~ [ or ] ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // Or is an LLVM IR or instruction.
 type Or struct {
+	// Name of local variable associated with the result.
+	LocalName string
+	// Operands.
+	X, Y value.Value
+}
+
+// NewOr returns a new or instruction based on the given operands.
+func NewOr(x, y value.Value) *Or {
+	return &Or{X: x, Y: y}
 }
 
 // ~~~ [ xor ] ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // Xor is an LLVM IR xor instruction.
 type Xor struct {
+	// Name of local variable associated with the result.
+	LocalName string
+	// Operands.
+	X, Y value.Value
+}
+
+// NewXor returns a new xor instruction based on the given operands.
+func NewXor(x, y value.Value) *Xor {
+	return &Xor{X: x, Y: y}
 }
---
+++ ir/instruction/instruction_test.go
@@ -0,0 +1,81 @@
+package instruction
+
+// Assert that each instruction implements the instruction.Instruction
+// interface.
+var (
+	// Binary instructions.
+	_ Instruction = (*Add)(nil)
+	_ Instruction = (*FAdd)(nil)
+	_ Instruction = (*Sub)(nil)
+	_ Instruction = (*FSub)(nil)
+	_ Instruction = (*Mul)(nil)
+	_ Instruction = (*FMul)(nil)
+	_ Instruction = (*UDiv)(nil)
+	_ Instruction = (*SDiv)(nil)
+	_ Instruction = (*FDiv)(nil)
+	_ Instruction = (*URem)(nil)
+	_ Instruction = (*SRem)(nil)
+	_ Instruction = (*FRem)(nil)
+	// Bitwise instructions.
+	_ Instruction = (*Shl)(nil)
+	_ Instruction = (*LShr)(nil)
+	_ Instruction = (*AShr)(nil)
+	_ Instruction = (*And)(nil)
+	_ Instruction = (*Or)(nil)
+	_ Instruction = (*Xor)(nil)
+	// Vector instructions.
+	_ Instruction = (*ExtractElement)(nil)
+	_ Instruction = (*InsertElement)(nil)
+	_ Instruction = (*ShuffleVector)(nil)
+	// Aggregate instructions.
+	_ Instruction = (*ExtractValue)(nil)
+	_ Instruction = (*InsertValue)(nil)
+	// Memory instructions.
+	_ Instruction = (*Alloca)(nil)
+	_ Instruction = (*Load)(nil)
+	_ Instruction = (*Store)(nil)
+	_ Instruction = (*Fence)(nil)
+	_ Instruction = (*CmpXchg)(nil)
+	_ Instruction = (*AtomicRMW)(nil)
+	_ Instruction = (*GetElementPtr)(nil)
+	// Conversion instructions.
+	_ Instruction = (*Trunc)(nil)
+	_ Instruction = (*ZExt)(nil)
+	_ Instruction = (*SExt)(nil)
+	_ Instruction = (*FPTrunc)(nil)
+	_ Instruction = (*FPExt)(nil)
+	_ Instruction = (*FPToUI)(nil)
+	_ Instruction = (*FPToSI)(nil)
+	_ Instruction = (*UIToFP)(nil)
+	_ Instruction = (*SIToFP)(nil)
+	_ Instruction = (*PtrToInt)(nil)
+	_ Instruction = (*IntToPtr)(nil)
+	_ Instruction = (*BitCast)(nil)
+	_ Instruction = (*AddrSpaceCast)(nil)
+	// Other instructions.
+	_ Instruction = (*ICmp)(nil)
+	_ Instruction = (*FCmp)(nil)
+	_ Instruction = (*Phi)(nil)
+	_ Instruction = (*Select)(nil)
+	_ Instruction = (*Call)(nil)
+	_ Instruction = (*VAArg)(nil)
+	_ Instruction = (*LandingPad)(nil)
+	_ Instruction = (*CatchPad)(nil)
+	_ Instruction = (*CleanupPad)(nil)
+)
+
+// Assert that each terminator implements the instruction.Terminator interface.
+var (
+	// Terminators.
+	_ Terminator = (*Ret)(nil)
+	_ Terminator = (*Br)(nil)
+	_ Terminator = (*CondBr)(nil)
+	_ Terminator = (*Switch)(nil)
+	_ Terminator = (*IndirectBr)(nil)
+	_ Terminator = (*Invoke)(nil)
+	_ Terminator = (*Resume)(nil)
+	_ Terminator = (*CatchSwitch)(nil)
+	_ Terminator = (*CatchRet)(nil)
+	_ Terminator = (*CleanupRet)(nil)
+	_ Terminator = (*Unreachable)(nil)
+)
