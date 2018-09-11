ee6450956ac132ee89d0ead3f98701146b34281a
Robin Eklind <rnd0x00@gmail.com>
2018-09-11 12:13:29 +0200

    ir: add constructors for aggregate instructions
    Former-commit-id: 0762495763eb309faff81b58fdc00958e76aa225

---
+++ ir/block_aggregate.go
@@ -0,0 +1,28 @@
+package ir
+
+import (
+	"github.com/llir/l/ir/instruction"
+	"github.com/llir/l/ir/value"
+)
+
+// --- [ Aggregate instructions ] ----------------------------------------------
+
+// ~~~ [ extractvalue ] ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// NewExtractValue returns a new extractvalue instruction based on the given
+// aggregate value and indicies.
+func (block *BasicBlock) NewExtractValue(x value.Value, indices []int64) *instruction.ExtractValue {
+	inst := instruction.NewExtractValue(x, indices)
+	block.Insts = append(block.Insts)
+	return inst
+}
+
+// ~~~ [ insertvalue ] ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// NewInsertValue returns a new insertvalue instruction based on the given
+// aggregate value, element and indicies.
+func (block *BasicBlock) NewInsertValue(x, elem value.Value, indices []int64) *instruction.InsertValue {
+	inst := instruction.NewInsertValue(x, elem, indices)
+	block.Insts = append(block.Insts)
+	return inst
+}
--- ir/instruction/inst_aggregate.go
+++ ir/instruction/inst_aggregate.go
@@ -1,15 +1,39 @@
 package instruction
 
+import "github.com/llir/l/ir/value"
+
 // --- [ Aggregate instructions ] ----------------------------------------------
 
 // ~~~ [ extractvalue ] ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // ExtractValue is an LLVM IR extractvalue instruction.
 type ExtractValue struct {
+	// Aggregate value.
+	X value.Value
+	// Indices.
+	Indices []int64
+}
+
+// NewExtractValue returns a new extractvalue instruction based on the given
+// aggregate value and indicies.
+func NewExtractValue(x value.Value, indices []int64) *ExtractValue {
+	return &ExtractValue{X: x, Indices: indices}
 }
 
 // ~~~ [ insertvalue ] ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // InsertValue is an LLVM IR insertvalue instruction.
 type InsertValue struct {
+	// Aggregate value.
+	X value.Value
+	// Element to insert.
+	Elem value.Value
+	// Indices.
+	Indices []int64
+}
+
+// NewInsertValue returns a new insertvalue instruction based on the given
+// aggregate value, element and indicies.
+func NewInsertValue(x, elem value.Value, indices []int64) *InsertValue {
+	return &InsertValue{X: x, Elem: elem, Indices: indices}
 }
