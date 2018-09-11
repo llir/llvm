c878e6b966c5b3f515c60c0736f8054376bd1bd0
Robin Eklind <rnd0x00@gmail.com>
2018-09-11 14:45:24 +0200

    ir: add constructors for other instructions

--- ir/instruction/inst_other.go
+++ ir/instruction/inst_other.go
@@ -1,57 +1,171 @@
 package instruction
 
+import (
+	"github.com/llir/l/ir/ll"
+	"github.com/llir/l/ir/types"
+	"github.com/llir/l/ir/value"
+)
+
 // --- [ Other instructions ] --------------------------------------------------
 
 // ~~~ [ icmp ] ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // ICmp is an LLVM IR icmp instruction.
 type ICmp struct {
+	// Name of local variable associated with the result.
+	LocalName string
+	// Integer comparison condition.
+	Cond ll.ICond
+	// Integer scalar or vector operands.
+	X, Y value.Value
+}
+
+// NewICmp returns a new icmp instruction based on the given integer comparison
+// condition and integer scalar or vector operands.
+func NewICmp(cond ll.ICond, x, y value.Value) *ICmp {
+	return &ICmp{Cond: cond, X: x, Y: y}
 }
 
 // ~~~ [ fcmp ] ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // FCmp is an LLVM IR fcmp instruction.
 type FCmp struct {
+	// Name of local variable associated with the result.
+	LocalName string
+	// Floating-point comparison condition.
+	Cond ll.FCond
+	// Floating-point scalar or vector operands.
+	X, Y value.Value
+}
+
+// NewFCmp returns a new fcmp instruction based on the given floating-point
+// comparison condition and floating-point scalar or vector operands.
+func NewFCmp(cond ll.FCond, x, y value.Value) *FCmp {
+	return &FCmp{Cond: cond, X: x, Y: y}
 }
 
 // ~~~ [ phi ] ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // Phi is an LLVM IR phi instruction.
 type Phi struct {
+	// Name of local variable associated with the result.
+	LocalName string
+	// Incoming values.
+	Incs []*ll.Incoming
+}
+
+// NewPhi returns a new phi instruction based on the given incoming values.
+func NewPhi(incs ...*ll.Incoming) *Phi {
+	return &Phi{Incs: incs}
 }
 
 // ~~~ [ select ] ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // Select is an LLVM IR select instruction.
 type Select struct {
+	// Name of local variable associated with the result.
+	LocalName string
+	// Selection condition.
+	Cond value.Value
+	// Operands.
+	X, Y value.Value
+}
+
+// NewSelect returns a new select instruction based on the given selection
+// condition and operands.
+func NewSelect(cond, x, y value.Value) *Select {
+	return &Select{Cond: cond, X: x, Y: x}
 }
 
 // ~~~ [ call ] ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // Call is an LLVM IR call instruction.
 type Call struct {
+	// Name of local variable associated with the result.
+	LocalName string
+	// Callee.
+	Callee value.Value
+	// Function arguments.
+	Args []ll.Arg
+}
+
+// NewCall returns a new call instruction based on the given callee and function
+// arguments.
+func NewCall(callee value.Value, args ...ll.Arg) *Call {
+	return &Call{Callee: callee, Args: args}
 }
 
 // ~~~ [ va_arg ] ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // VAArg is an LLVM IR va_arg instruction.
 type VAArg struct {
+	// Name of local variable associated with the result.
+	LocalName string
+	// Variable argument list.
+	List value.Value
+	// Argument type.
+	ArgType types.Type
+}
+
+// NewVAArg returns a new va_arg instruction based on the given variable
+// argument list and argument type.
+func NewVAArg(list value.Value, argType types.Type) *VAArg {
+	return &VAArg{List: list, ArgType: argType}
 }
 
 // ~~~ [ landingpad ] ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // LandingPad is an LLVM IR landingpad instruction.
 type LandingPad struct {
+	// Name of local variable associated with the result.
+	LocalName string
+	// Result type.
+	ResultType types.Type
+	// Cleanup landing pad.
+	Cleanup bool
+	// Filter and catch clauses; zero or more if Cleanup is true, otherwise one
+	// or more.
+	Clauses []*ll.Clause
+}
+
+// NewLandingPad returns a new landingpad instruction based on the given filter
+// and catch clauses.
+func NewLandingPad(clauses ...*ll.Clause) *LandingPad {
+	return &LandingPad{Clauses: clauses}
 }
 
 // ~~~ [ catchpad ] ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // CatchPad is an LLVM IR catchpad instruction.
 type CatchPad struct {
+	// Name of local variable associated with the result.
+	LocalName string
+	// Exception scope.
+	Scope value.Value // TODO: assess if Scope is a good name. figure out the specific set of underlying types.
+	// Exception arguments.
+	Args []ll.Arg
+}
+
+// NewCatchPad returns a new catchpad instruction based on the given exception
+// scope and exception arguments.
+func NewCatchPad(scope value.Value, args ...ll.Arg) *CatchPad {
+	return &CatchPad{Scope: scope, Args: args}
 }
 
 // ~~~ [ cleanuppad ] ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // CleanupPad is an LLVM IR cleanuppad instruction.
 type CleanupPad struct {
+	// Name of local variable associated with the result.
+	LocalName string
+	// Exception scope.
+	Scope ll.ExceptionScope // TODO: rename to parent?
+	// Exception arguments.
+	Args []ll.Arg
+}
+
+// NewCleanupPad returns a new cleanuppad instruction based on the given
+// exception scope and exception arguments.
+func NewCleanupPad(scope ll.ExceptionScope, args ...ll.Arg) *CleanupPad {
+	return &CleanupPad{Scope: scope, Args: args}
 }
--- ir/ll/ll.go
+++ ir/ll/ll.go
@@ -2,6 +2,31 @@
 
 package ll
 
+import "github.com/llir/l/ir/value"
+
+type Arg interface {
+	isArg()
+}
+
 type AtomicOp uint
 
 type AtomicOrdering uint
+
+type Clause struct {
+}
+
+type ExceptionScope interface {
+	isExceptionScope()
+}
+
+type FCond uint
+
+type ICond uint
+
+// Incoming is an incoming value of a phi instruction.
+type Incoming struct {
+	// Incoming value.
+	X value.Value
+	// Predecessor basic block of the incoming value.
+	Pred value.Value // *ir.BasicBlock
+}
