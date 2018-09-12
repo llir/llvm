64c774650df8814837c8cf7954e565a6817ee49b
Robin Eklind <rnd0x00@gmail.com>
2018-09-13 00:59:11 +0200

    internal/enc: copy from llir/llvm/internal/enc@7a17b32c1767cfeb5287d164e92865adb98985c8

---
+++ internal/enc/enc_test.go
@@ -0,0 +1,214 @@
+package enc
+
+import (
+	"testing"
+)
+
+func TestGlobal(t *testing.T) {
+	golden := []struct {
+		s    string
+		want string
+	}{
+		// i=0
+		{s: "foo", want: "@foo"},
+		// i=1
+		{s: "a b", want: `@"a\20b"`},
+		// i=2
+		{s: "$a", want: "@$a"},
+		// i=3
+		{s: "-a", want: "@-a"},
+		// i=4
+		{s: ".a", want: "@.a"},
+		// i=5
+		{s: "_a", want: "@_a"},
+		// i=6
+		{s: "#a", want: `@"\23a"`},
+		// i=7
+		{s: "a b#c", want: `@"a\20b\23c"`},
+		// i=8
+		{s: "2", want: "@2"},
+		// i=9
+		{s: "foo世bar", want: `@"foo\E4\B8\96bar"`},
+	}
+
+	for i, g := range golden {
+		got := Global(g.s)
+		if got != g.want {
+			t.Errorf("i=%d: name mismatch; expected %q, got %q", i, g.want, got)
+		}
+	}
+}
+
+func TestLocal(t *testing.T) {
+	golden := []struct {
+		s    string
+		want string
+	}{
+		// i=0
+		{s: "foo", want: "%foo"},
+		// i=1
+		{s: "a b", want: `%"a\20b"`},
+		// i=2
+		{s: "$a", want: "%$a"},
+		// i=3
+		{s: "-a", want: "%-a"},
+		// i=4
+		{s: ".a", want: "%.a"},
+		// i=5
+		{s: "_a", want: "%_a"},
+		// i=6
+		{s: "#a", want: `%"\23a"`},
+		// i=7
+		{s: "a b#c", want: `%"a\20b\23c"`},
+		// i=8
+		{s: "2", want: "%2"},
+		// i=9
+		{s: "foo世bar", want: `%"foo\E4\B8\96bar"`},
+	}
+
+	for i, g := range golden {
+		got := Local(g.s)
+		if got != g.want {
+			t.Errorf("i=%d: name mismatch; expected %q, got %q", i, g.want, got)
+		}
+	}
+}
+
+func TestMetadata(t *testing.T) {
+	golden := []struct {
+		s    string
+		want string
+	}{
+		// i=0
+		{s: "foo", want: "!foo"},
+		// i=1
+		{s: "a b", want: `!a\20b`},
+		// i=2
+		{s: "$a", want: "!$a"},
+		// i=3
+		{s: "-a", want: "!-a"},
+		// i=4
+		{s: ".a", want: "!.a"},
+		// i=5
+		{s: "_a", want: "!_a"},
+		// i=6
+		{s: "#a", want: `!\23a`},
+		// i=7
+		{s: "a b#c", want: `!a\20b\23c`},
+		// i=8
+		{s: "2", want: "!2"},
+		// i=9
+		{s: "foo世bar", want: `!foo\E4\B8\96bar`},
+	}
+
+	for i, g := range golden {
+		got := Metadata(g.s)
+		if got != g.want {
+			t.Errorf("i=%d: name mismatch; expected %q, got %q", i, g.want, got)
+		}
+	}
+}
+
+func TestUnescape(t *testing.T) {
+	golden := []struct {
+		s    string
+		want string
+	}{
+		// i=0
+		{s: "foo", want: "foo"},
+		// i=1
+		{s: `a\20b`, want: "a b"},
+		// i=2
+		{s: "$a", want: "$a"},
+		// i=3
+		{s: "-a", want: "-a"},
+		// i=4
+		{s: ".a", want: ".a"},
+		// i=5
+		{s: "_a", want: "_a"},
+		// i=6
+		{s: `\23a`, want: "#a"},
+		// i=7
+		{s: `a\20b\23c`, want: "a b#c"},
+		// i=8
+		{s: "2", want: "2"},
+		// i=9
+		{s: `foo\E4\B8\96bar`, want: "foo世bar"},
+		// i=10
+		{s: `foo \5C bar`, want: `foo \ bar`},
+		// i=11
+		{s: `foo \\ bar`, want: `foo \ bar`},
+	}
+
+	for i, g := range golden {
+		got := Unescape(g.s)
+		if got != g.want {
+			t.Errorf("i=%d: string mismatch; expected %q, got %q", i, g.want, got)
+		}
+	}
+}
+
+func TestEscape(t *testing.T) {
+	golden := []struct {
+		s    string
+		want string
+	}{
+		// i=0
+		{s: "foo", want: "foo"},
+		// i=1
+		{s: "a b", want: `a b`},
+		// i=2
+		{s: "$a", want: "$a"},
+		// i=3
+		{s: "-a", want: "-a"},
+		// i=4
+		{s: ".a", want: ".a"},
+		// i=5
+		{s: "_a", want: "_a"},
+		// i=6
+		{s: "#a", want: `#a`},
+		// i=7
+		{s: "a b#c", want: `a b#c`},
+		// i=8
+		{s: "2", want: "2"},
+		// i=9
+		{s: "foo世bar", want: `foo\E4\B8\96bar`},
+		// i=10
+		{s: `foo \ bar`, want: `foo \5C bar`},
+	}
+
+	// isPrint reports whether the given byte is printable.
+	isPrint := func(b byte) bool {
+		return ' ' <= b && b <= '~' && b != '"' && b != '\\'
+	}
+	for i, g := range golden {
+		got := Escape(g.s, isPrint)
+		if got != g.want {
+			t.Errorf("i=%d: string mismatch; expected %q, got %q", i, g.want, got)
+		}
+	}
+}
+
+func BenchmarkGlobalNoReplace(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Global("$foo_bar_baz")
+	}
+}
+
+func BenchmarkGlobalReplace(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Global("$foo bar#baz")
+	}
+}
+
+func BenchmarkLocalNoReplace(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Local("$foo_bar_baz")
+	}
+}
+
+func BenchmarkLocalReplace(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Local("$foo bar#baz")
+	}
+}
