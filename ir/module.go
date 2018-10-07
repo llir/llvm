50f33ef0f5c32b87ef69df42326c5d6f69c93b04
Robin Eklind <rnd0x00@gmail.com>
2018-10-07 03:06:03 +0200

    ir: add SourceFilename to Module
    Former-commit-id: 31e24a3e7a2b61d2e981af92c6446db3e6f6ef04

--- ir/module.go
+++ ir/module.go
@@ -18,6 +18,36 @@ type Module struct {
 	Globals []*Global
 	// Function declarations and definitions.
 	Funcs []*Function
+
+	// extra.
+
+	// (optional) Source filename; or empty if not present.
+	SourceFilename string
+	/*
+		// (optional) Data layout; or empty if not present.
+		DataLayout string
+		// (optional) Target triple; or empty if not present.
+		TargetTriple string
+		// (optional) Module-level inline assembly.
+		ModuleAsms []string
+		// (optional) Comdat definitions.
+		ComdatDefs []*ll.ComdatDef
+		// (optional) Indirect symbol definitions (aliases and IFuncs).
+		// TODO: figure out how to represent aliases and IFuncs.
+		//IndirectSymbols []*IndirectSymbol
+		// (optional) Attribute group definitions.
+		AttrGroupDefs []*ll.AttrGroupDef
+		// (optional) Named metadata definitions.
+		// TODO: figure out how to represent metadata.
+		//NamedMetadataDefs []*metadata.NamedMetadataDef
+		// (optional) Metadata definitions.
+		// TODO: figure out how to represent metadata.
+		//MetadataDefs []*metadata.MetadataDef
+		// (optional) Use-list order directives.
+		UseListOrders []*ll.UseListOrder
+		// (optional) Basic block specific use-list order directives.
+		UseListOrderBBs []*UseListOrderBB
+	*/
 }
 
 // Def returns the LLVM syntax representation of the module.
