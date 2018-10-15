f69ef191dd0af175c7628b4fa7b7d57778070b7f
Robin Eklind <rnd0x00@gmail.com>
2018-10-15 20:15:58 +0200

    asm: add scaffolding for Translate
    Note, this is the first of potential several attempted
    approaches at finding a clean and performant way of
    resolving identifiers, translating from AST to IR form
    and validating the IR.
    
    We iterate and learn until we arrive at a successful
    approach for doing this.

--- cmd/l-tm/main.go
+++ cmd/l-tm/main.go
@@ -6,8 +6,8 @@ import (
 	"log"
 	"time"
 
+	"github.com/kr/pretty"
 	"github.com/mewmew/l-tm/asm"
-	"github.com/mewmew/l-tm/asm/ll/ast"
 )
 
 func main() {
@@ -22,20 +22,7 @@ func main() {
 		}
 		fmt.Println("took:", time.Since(start))
 		fmt.Println()
-		fmt.Println("module:", module.Text())
-		for _, entity := range module.TopLevelEntities() {
-			fmt.Printf("entity %T: %v\n", entity, entity.Text())
-			switch entity := entity.(type) {
-			case *ast.SourceFilename:
-				fmt.Println("   name:", entity.Name().Text())
-			case *ast.TargetDataLayout:
-				fmt.Println("   datalayout:", entity.DataLayout().Text())
-			case *ast.TargetTriple:
-				fmt.Println("   target triple:", entity.TargetTriple().Text())
-			case *ast.ModuleAsm:
-				fmt.Println("   module asm:", entity.Asm().Text())
-			}
-		}
-		fmt.Println()
+		m := asm.Translate(module)
+		pretty.Println(m)
 	}
 }
