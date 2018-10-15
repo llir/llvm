217eedfeb8ce8e2147c25c3fc4acfc36c8b7a839
Robin Eklind <rnd0x00@gmail.com>
2018-10-16 00:40:28 +0200

    cmd/l-tm: add -types flag to enable/diable type resolution of type definitions
    Former-commit-id: 96d25daf299e8251dda94169c6bfc168bf42250d

--- cmd/l-tm/main.go
+++ cmd/l-tm/main.go
@@ -6,26 +6,29 @@ import (
 	"log"
 	"time"
 
-	"github.com/kr/pretty"
 	"github.com/mewmew/l-tm/asm"
 )
 
 func main() {
+	flag.BoolVar(&asm.DoTypeResolution, "types", true, "enable type resolution of type definitions")
 	flag.Parse()
 	for _, llPath := range flag.Args() {
 		fmt.Printf("=== [ %v ] =======================\n", llPath)
 		fmt.Println()
-		start := time.Now()
+		fileStart := time.Now()
+		parseStart := time.Now()
 		module, err := asm.ParseFile(llPath)
 		if err != nil {
 			log.Fatalf("%q: %+v", llPath, err)
 		}
-		fmt.Println("parsing into AST took:", time.Since(start))
+		fmt.Println("parsing into AST took:", time.Since(parseStart))
 		fmt.Println()
 		m, err := asm.Translate(module)
 		if err != nil {
 			log.Fatalf("%q: %+v", llPath, err)
 		}
-		pretty.Println(m)
+		_ = m
+		//pretty.Println(m)
+		fmt.Printf("total time for file %q: %v\n", llPath, time.Since(fileStart))
 	}
 }
