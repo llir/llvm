3f7c8740681f73ba2abd4d9e0ce2c01edc556b81
Robin Eklind <rnd0x00@gmail.com>
2018-10-15 21:02:19 +0200

    asm: add scaffolding for translating type definitions

--- asm/translate.go
+++ asm/translate.go
@@ -2,17 +2,45 @@ package asm
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/llir/l/ir"
 	"github.com/mewmew/l-tm/asm/ll/ast"
+	"github.com/mewmew/l-tm/internal/enc"
 )
 
 // Translate translates the AST of the given module to an equivalent LLVM IR
 // module.
 func Translate(module *ast.Module) *ir.Module {
 	m := &ir.Module{}
+	// Translate type definitions.
 	for _, entity := range module.TopLevelEntities() {
-		fmt.Printf("entity: %T\n", entity)
+		switch entity := entity.(type) {
+		case *ast.TypeDef:
+			fmt.Println("alias:", local(entity.Alias()))
+		default:
+			fmt.Printf("entity: %T\n", entity)
+		}
 	}
 	return m
+}
+
+// local returns the name of the local identifier.
+func local(l ast.LocalIdent) string {
+	text := l.Text()
+	const prefix = "%"
+	if !strings.HasPrefix(text, prefix) {
+		panic(fmt.Errorf("invalid local identifier `%s`; missing '%s' prefix", text, prefix))
+	}
+	text = text[len(prefix):]
+	return unquote(text)
+}
+
+// unquote returns the unquoted version of s if quoted, and the original string
+// otherwise.
+func unquote(s string) string {
+	if len(s) >= 2 && strings.HasPrefix(s, `"`) && strings.HasSuffix(s, `"`) {
+		return string(enc.Unquote(s))
+	}
+	return s
 }
