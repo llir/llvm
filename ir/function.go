7e9a10fa5ba485f69b2c2096e9ca55d8c0a084d0
Robin Eklind <rnd0x00@gmail.com>
2018-10-16 18:41:56 +0200

    ir: add extra fields to Global

--- ir/function.go
+++ ir/function.go
@@ -11,22 +11,68 @@ import (
 
 // Function is an LLVM IR function.
 type Function struct {
-	// Return type.
-	RetType types.Type
+	// Function signature.
+	Sig *types.FuncType
 	// Function name.
 	FuncName string
 	// Function parameters.
 	Params []*Param
-	// Variable number of arguments.
-	Variadic bool
 	// Basic blocks.
 	Blocks []*BasicBlock
+
+	/*
+		// (optional) Linkage.
+		Linkage ll.Linkage
+		// (optional) Preemption; zero value if not present.
+		Preemption ll.Preemption
+		// (optional) Visibility; zero value if not present.
+		Visibility ll.Visibility
+		// (optional) DLL storage class; zero value if not present.
+		DLLStorageClass ll.DLLStorageClass
+		// (optional) Calling convention; zero value if not present.
+		CallingConv ll.CallingConv
+		// (optional) Return attributes.
+		ReturnAttrs []ll.ReturnAttribute
+		// (optional) Unnamed address.
+		UnnamedAddr ll.UnnamedAddr
+		// (optional) Function attributes.
+		FuncAttrs []ll.FuncAttribute
+		// (optional) Section; nil if not present.
+		Section *ll.Section
+		// (optional) Comdat; nil if not present.
+		Comdat *ll.Comdat
+		// (optional) Garbage collection; empty if not present.
+		GC string
+		// (optional) Prefix; nil if not present.
+		Prefix Constant
+		// (optional) Prologue; nil if not present.
+		Prologue Constant
+		// (optional) Personality; nil if not present.
+		Personality Constant
+		// (optional) Use list order.
+		UseListOrders []*UseListOrder
+		// (optional) Metadata attachments.
+		// TODO: add support for metadata.
+		//Metadata []*metadata.MetadataAttachment
+	*/
 }
+
+// TODO: decide whether to have the function name parameter be the first
+// argument (to be consistent with NewGlobal) or after retType (to be consistent
+// with order of occurence in LLVM IR syntax).
 
 // NewFunction returns a new function based on the given function name, return
 // type and function parameters.
 func NewFunction(name string, retType types.Type, params ...*Param) *Function {
-	return &Function{FuncName: name, RetType: retType, Params: params}
+	panic("not yet implemented")
+	/*
+		paramTypes := make([]types.Type, len(params))
+		for i, param := range f.Params {
+			paramType[i] = param.Type()
+		}
+		sig := types.NewFunc(f.RetType, paramTypes...)
+		return &Function{Sig: sig, FuncName: name, Params: params}
+	*/
 }
 
 // String returns the LLVM syntax representation of the function as a type-value
@@ -38,12 +84,7 @@ func (f *Function) String() string {
 // Type returns the type of the function.
 func (f *Function) Type() types.Type {
 	// TODO: cache type?
-	sig := types.NewFunc(f.RetType)
-	for _, param := range f.Params {
-		sig.Params = append(sig.Params, param.Typ)
-	}
-	sig.Variadic = f.Variadic
-	return types.NewPointer(sig)
+	return types.NewPointer(f.Sig)
 }
 
 // Ident returns the identifier associated with the function.
--- ir/global.go
+++ ir/global.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/llir/l/internal/enc"
+	"github.com/llir/l/ir/ll"
 	"github.com/llir/l/ir/types"
 )
 
@@ -19,6 +20,37 @@ type Global struct {
 	ContentType types.Type
 	// Initial value; or nil if declaration.
 	Init Constant
+
+	// extra.
+
+	// Pointer type to global variable, including optional address space.
+	Typ *types.PointerType
+	// (optional) Linkage.
+	Linkage ll.Linkage
+	// (optional) Preemption; zero value if not present.
+	Preemption ll.Preemption
+	// (optional) Visibility; zero value if not present.
+	Visibility ll.Visibility
+	// (optional) DLL storage class; zero value if not present.
+	DLLStorageClass ll.DLLStorageClass
+	// (optional) Thread local storage model; zero value if not present.
+	TLSModel ll.TLSModel
+	// (optional) Unnamed address.
+	UnnamedAddr ll.UnnamedAddr
+	// (optional) Externally initialized.
+	ExternallyInitialized bool
+	// (optional) Section name; empty if not present.
+	Section string
+	// (optional) Comdat definition; nil if not present.
+	// TODO: define ComdatDef.
+	//Comdat *ComdatDef
+	// (optional) Alignment; zero if not present.
+	Alignment int
+	// (optional) Function attributes.
+	FuncAttrs []ll.FuncAttribute
+	// (optional) Metadata attachments.
+	// TODO: add support for metadata.
+	//Metadata []*metadata.MetadataAttachment
 }
 
 // NewGlobalDecl returns a new global variable declaration based on the given
@@ -41,8 +73,11 @@ func (g *Global) String() string {
 
 // Type returns the type of the global variable.
 func (g *Global) Type() types.Type {
-	// TODO: cache type?
-	return types.NewPointer(g.ContentType)
+	// Cache type if not present.
+	if g.Typ == nil {
+		g.Typ = types.NewPointer(g.ContentType)
+	}
+	return g.Typ
 }
 
 // Ident returns the identifier associated with the global variable.
