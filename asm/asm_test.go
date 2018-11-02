6433f6fe0b939ad62ea0d3a734a2a6dc315ef026
Robin Eklind <rnd0x00@gmail.com>
2018-11-02 03:44:38 +0100

    asm: add test cases for binary instructions
    Former-commit-id: 83d78705b7c8c98b3ce994fbd7b8c0591b1de0a9

---
+++ asm/asm_test.go
@@ -0,0 +1,51 @@
+package asm
+
+import (
+	"io/ioutil"
+	"testing"
+)
+
+func TestParseFile(t *testing.T) {
+	golden := []struct {
+		path string
+	}{
+		{path: "testdata/inst_binary.ll"},
+	}
+	for _, g := range golden {
+		_, err := ParseFile(g.path)
+		if err != nil {
+			t.Errorf("unable to parse %q into AST; %v", g.path, err)
+			continue
+		}
+	}
+}
+
+func TestTranslate(t *testing.T) {
+	golden := []struct {
+		path string
+	}{
+		{path: "testdata/inst_binary.ll"},
+	}
+	for _, g := range golden {
+		m, err := ParseFile(g.path)
+		buf, err := ioutil.ReadFile(g.path)
+		if err != nil {
+			t.Errorf("unable to read %q; %v", g.path, err)
+		}
+		want := string(buf)
+		if err != nil {
+			t.Errorf("unable to parse %q into AST; %v", g.path, err)
+			continue
+		}
+		module, err := Translate(m)
+		if err != nil {
+			t.Errorf("unable to translate %q from AST to IR; %v", g.path, err)
+			continue
+		}
+		got := module.Def()
+		if want != got {
+			t.Errorf("module mismatch; expected `%s`, got `%s`", want, got)
+			continue
+		}
+	}
+}
