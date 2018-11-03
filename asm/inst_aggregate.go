69e88259346fe2575e3ebd86f9f760df3cba80c7
Robin Eklind <rnd0x00@gmail.com>
2018-11-03 03:01:09 +0100

    asm: translate aggregate instructions
    Former-commit-id: 120abccca21b282817f51c1a78b09e79c08370f1

--- asm/inst_aggregate.go
+++ asm/inst_aggregate.go
@@ -3,8 +3,9 @@ package asm
 import (
 	"fmt"
 
-	"github.com/llir/llvm/ir"
 	"github.com/llir/ll/ast"
+	"github.com/llir/llvm/ir"
+	"github.com/pkg/errors"
 )
 
 // --- [ Aggregate instructions ] ----------------------------------------------
@@ -14,10 +15,20 @@ import (
 func (fgen *funcGen) astToIRInstExtractValue(inst ir.Instruction, old *ast.ExtractValueInst) (*ir.InstExtractValue, error) {
 	i, ok := inst.(*ir.InstExtractValue)
 	if !ok {
-		// NOTE: panic since this would indicate a bug in the implementation.
 		panic(fmt.Errorf("invalid IR instruction for AST instruction; expected *ir.InstExtractValue, got %T", inst))
 	}
-	// TODO: implement
+	// Aggregate value.
+	x, err := fgen.astToIRTypeValue(old.X())
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	i.X = x
+	// Element indices.
+	indices := uintSlice(old.Indices())
+	for _, index := range indices {
+		i.Indices = append(i.Indices, int64(index))
+	}
+	// TODO: add support for metadata.
 	return i, nil
 }
 
@@ -26,9 +37,25 @@ func (fgen *funcGen) astToIRInstExtractValue(inst ir.Instruction, old *ast.Extra
 func (fgen *funcGen) astToIRInstInsertValue(inst ir.Instruction, old *ast.InsertValueInst) (*ir.InstInsertValue, error) {
 	i, ok := inst.(*ir.InstInsertValue)
 	if !ok {
-		// NOTE: panic since this would indicate a bug in the implementation.
 		panic(fmt.Errorf("invalid IR instruction for AST instruction; expected *ir.InstInsertValue, got %T", inst))
 	}
-	// TODO: implement
+	// Aggregate value.
+	x, err := fgen.astToIRTypeValue(old.X())
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	i.X = x
+	// Element to insert.
+	elem, err := fgen.astToIRTypeValue(old.Elem())
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	i.Elem = elem
+	// Element indices.
+	indices := uintSlice(old.Indices())
+	for _, index := range indices {
+		i.Indices = append(i.Indices, int64(index))
+	}
+	// TODO: add support for metadata.
 	return i, nil
 }
