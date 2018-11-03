49ad0c97a2d2065f0ee7a7f471f6b9ffba4bd656
Robin Eklind <rnd0x00@gmail.com>
2018-11-03 02:19:21 +0100

    cmd/l-tm: add l-tm command to facilitate development
    This command will be removed prior to the next release.

---
+++ cmd/l-tm/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/llir/llvm/asm"
+)
+
+func main() {
+	flag.BoolVar(&asm.DoTypeResolution, "types", true, "enable type resolution of type definitions")
+	flag.BoolVar(&asm.DoGlobalResolution, "globals", true, "enable global resolution of global variable and function declarations and definitions")
+	flag.Parse()
+	for _, llPath := range flag.Args() {
+		fmt.Printf("=== [ %v ] =======================\n", llPath)
+		fmt.Println()
+		fileStart := time.Now()
+		parseStart := time.Now()
+		module, err := asm.ParseFile(llPath)
+		if err != nil {
+			log.Fatalf("%q: %+v", llPath, err)
+		}
+		fmt.Println("parsing into AST took:", time.Since(parseStart))
+		fmt.Println()
+		m, err := asm.Translate(module)
+		if err != nil {
+			log.Fatalf("%q: %+v", llPath, err)
+		}
+		_ = m
+		//pretty.Println(m)
+		fmt.Printf("total time for file %q: %v\n", llPath, time.Since(fileStart))
+	}
+}
