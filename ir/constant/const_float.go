a7a3a27d0ef458826ba515bac13e874b13236b79
Robin Eklind <rnd0x00@gmail.com>
2018-11-10 05:33:07 +0100

    ir/constant: add (preliminary) hex float64 support

--- ir/constant/const_float.go
+++ ir/constant/const_float.go
@@ -183,12 +183,19 @@ func (c *Float) Ident() string {
 		// last 29 bits of significand will always be ignored.  As an
 		// error-detection measure, the IR parser requires them to be zero.
 		if !exact32(c.X) {
-			f, acc := c.X.Float64()
-			// TODO: check acc.
-			_ = acc
+			f, _ := c.X.Float64()
 			x := math.Float64bits(f)
 			x &= 0xFFFFFFFFE0000000 // clear 29 least significant bits.
 			return fmt.Sprintf("0x%016X", x)
+		}
+	case types.FloatKindDouble:
+		if !exact64(c.X) {
+			f, _ := c.X.Float64()
+			x := math.Float64bits(f)
+			// Note, to match Clang output we do not zero-pad the hexadecimal
+			// output.
+			return fmt.Sprintf("0x%X", x)
+			//return fmt.Sprintf("0x%016X", x)
 		}
 	}
 
@@ -206,14 +213,45 @@ func (c *Float) Ident() string {
 	return s
 }
 
-// exact32 reports whether the given floating-point value may be represented
-// exactly as a 32-bit floating-point value.
+// exact32 reports whether x may be represented exactly as a float32,
 func exact32(x *big.Float) bool {
-	f1, _ := x.Float32()
-	s1 := strconv.FormatFloat(float64(f1), 'g', -1, 32)
-	f2, _ := x.Float64()
-	s2 := strconv.FormatFloat(f2, 'g', -1, 64)
-	fmt.Println("s1:", s1)
-	fmt.Println("s2:", s2)
+	f, acc := x.Float32()
+	if acc != big.Exact {
+		return false
+	}
+	s1 := strconv.FormatFloat(float64(f), 'e', -1, 32)
+	s2 := trimZeros(x.Text('e', 100))
 	return s1 == s2
+}
+
+// exact64 reports whether x may be represented exactly as a float64,
+func exact64(x *big.Float) bool {
+	f, acc := x.Float64()
+	if acc != big.Exact {
+		return false
+	}
+	s1 := strconv.FormatFloat(f, 'e', -1, 64)
+	s2 := trimZeros(x.Text('e', 100))
+	return s1 == s2
+}
+
+// trimZeros trims trailing zeroes after the decimal point in the given
+// floating-point value (represented in scientific notation). If all digits
+// after the decimal point are trimmed this way, the decimal point is also
+// trimmed.
+func trimZeros(s string) string {
+	epos := strings.Index(s, "e")
+	if epos == -1 {
+		panic(fmt.Errorf("unable to locate position of exponent (e.g. e+02) in %q", s))
+	}
+	pos := epos - 1
+	for ; pos > 0; pos-- {
+		if s[pos] != '0' {
+			break
+		}
+	}
+	if s[pos] != '.' {
+		pos++
+	}
+	return fmt.Sprintf("%s%s", s[:pos], s[epos:])
 }
