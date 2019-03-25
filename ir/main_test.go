49a37090f32979cc3137487a025ec6ee10eb8bcf
Theo <theo.despoudis@teckro.com>
2019-03-25 19:18:27 +0000

    Updated main_test

--- ir/main_test.go
+++ ir/main_test.go
@@ -23,31 +23,25 @@ func Example_main() {
 	//define i32 @main() {
 	//; <label>:0
 	//       %1 = alloca i32, align 4
-	//       %2 = alloca i32, align 4
-	//       %3 = alloca i32, align 4
-	//       store i32 0, i32* %1, align 4
-	//       store i32 32, i32* %2, align 4
-	//       store i32 16, i32* %3, align 4
-	//       %4 = load i32, i32* %2, align 4
-	//       %5 = load i32, i32* %3, align 4
-	//       %6 = add i32 %4, %5
-	//       ret i32 %6
+	//		 %2 = alloca i32, align 4
+	//		 store i32 32, i32* %1, align 4
+	//	     store i32 16, i32* %2, align 4
+	//	     %3 = load i32, i32* %1, align 4
+	//	     %4 = load i32, i32* %2, align 4
+	//	     %5 = add i32 %3, %4
+	//	     ret i32 %5
 	//}
 
 	i32 := types.I32
 
 	// Create a new LLVM IR module.
-	mod := ir.NewModule()
+	m := ir.NewModule()
 	//    int main() { ... }
-	main := mod.NewFunc("main", i32)
+	main := m.NewFunc("main", i32)
 
 	// Create an unnamed entry basic block and append it to the `main` function.
 	entry := main.NewBlock("")
-	// Create instructions and append them to the entry basic
-
-	// %1 = alloca i32, align 4
-	zero := entry.NewAlloca(i32)
-	zero.Align = 4
+	// Create instructions and append them to the entry basic block
 
 	// %a = alloca i32, align 4
 	a := entry.NewAlloca(i32)
@@ -57,31 +51,28 @@ func Example_main() {
 	b := entry.NewAlloca(i32)
 	b.Align = 4
 
-	// store i32 0, i32* %1
-	s0 := entry.NewStore(constant.NewInt(i32, 0), zero)
-	s0.Align = 4
-
-	// store i32 32, i32* %a, align 4
+	// store i32 32, i32* %1, align 4
 	s1 := entry.NewStore(constant.NewInt(i32, 32), a)
 	s1.Align = 4
 
-	// store i32 16, i32* %b, align 4
+	// store i32 16, i32* %2, align 4
 	s2 := entry.NewStore(constant.NewInt(i32, 16), b)
 	s2.Align = 4
 
-	// %2 = load i32, i32* %a, align 4
+	// %2 = load i32, i32* %1, align 4
 	tmpA := entry.NewLoad(a)
 	tmpA.Align = 4
 
-	// %3 = load i32, i32* %b, align 4
+	// %3 = load i32, i32* %2, align 4
 	tmpB := entry.NewLoad(b)
 	tmpB.Align = 4
 
 	// %4 = add nsw i32 %2, %3
 	tmpC := entry.NewAdd(tmpA, tmpB)
 
+	// ret i32 %5
 	entry.NewRet(tmpC)
 
 	// Print the LLVM IR assembly of the module.
-	fmt.Println(mod)
+	fmt.Println(m)
 }
