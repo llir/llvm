d460b72b81c3a074f8a8f31e65e320a366c08a18
Robin Eklind <mewmew@users.noreply.github.com>
2020-01-08 21:52:04 +0100

    ir/constant: output int const in hex notation if more readable (#126)
    * ir/constant: add test case for hex vs. decimal integer notation
    
    Updates #125.
    
    * ir/constant: output int const in hex notation if more readable
    
    To improve readability of integer constants in LLVM IR output, a notion
    of "low entropy" is used to determine whether to output integer
    constants in decimal or hexadecimal notation.
    
    The main idea is to check how many unique digits are required for each
    representation and use the notation (hex or decimal) that requires the
    least.
    
    To illustrate this idea, here are a few examples from before and after
    this commit.
    
    Before: 2147483648
    After:  u0x80000000
    
    Before: 9218868437227405312
    After:  u0x7FF0000000000000
    
    For now, we have a threshold at 1000, so integer constants below 1000
    are always represented in decimal notation. This includes negative
    integer values.
    
    Fixes #125.
    
    * ir/constant: refine notion of entropy to unique_digits/total_digits
    
    Prior to this commit, the length of an integer constant was not
    considered when computing its entropy. Thus long hexadecimal literals
    got an unfair advantage as each of their digits contained more
    information than their decimal counterpart.
    
    The new definition of entropy is always in range (0.0, 1.0]. That is,
    it is always above 0.0 and below or equal to 1.0.
    
    Also, refine lower threshold to 0x1000 (instead of 1000).
    
    If decimal notation require 0.3 or more entropy than hex notation, then
    we output in hex.
    
    * ir/constant: cap length by base when computing entropy
    
    Without this fix, longer integer constants would get lower entropy even
    though they may contain a lot of different digits.
    
    For instance, the integer constant 1234567890 would get 1.0 as entropy,
    while 12345678901234567890 would get 0.5 (since it contains every
    digit, 10 and has a length of 20).
    
    With this fix 12345678901234567890 has entropy 1.0, as should be, since
    length is now capped by the base (10 for decimal).
    
    * ir/constnat: add max hex entropy check
    
    	Maximum allowed entropy of hexadecimal notation to output x in hexadecimal
    	notation.
    
    	This is useful as some hex values, while lower entropy than their decimal
    	counter-part do not improve readability.
    
    	For instance, the decimal entropy of 7240739780546808700 is 9/10 = 0.9 and
    	the hexadecimal entropy of 0x647C4677A2884B7C is 8/16 = 0.5. As such the
    	entropy difference is 0.9-0.5 = 0.4, but the hexadecimal notation does not
    	improve readability. Thus we add an upper bound on the hexadecimal entropy,
    	and if the entropy is above this bound, output in decimal notation
    	instead.
    
    * ir/constant: fine-tune calcMaxHexEntropy and set minEntropyDiff = 0.2
    
    This seems to be a good balance between finding opportunities to print
    values such as 0x80000 in hex while keeping values such as
    0x6160ff9e9f006161 in decimal form, as they don't improve readability
    enough to justify hex notation.
    
    Now printed as hex:
    	(unique hex digits: 2) hex: (length=5) 0x80000 | dec: 524288 (length=6) | hex entropy: 0.4 (max hex entropy: 0.4) | dec entropy: 0.6666666666666666 (unique digits: 4) |    ---> print as hex: 0x80000
    
    Now printed as dec:
    	(unique hex digits: 6) hex: (length=16) 0x6160ff9e9f006161 | dec: 7016889276180750689 (length=19) | hex entropy: 0.375 (max hex entropy: 0.375) | dec entropy: 0.8 (unique dec digits: 8) |    ---> print as dec: 7016889276180750689
    
    * ir/constnat: update comment of calcMaxHexEntropy
    
    * testdata: update to latest commit

--- ir/constant/const_int.go
+++ ir/constant/const_int.go
@@ -127,5 +127,175 @@ func (c *Int) Ident() string {
 			panic(fmt.Errorf("invalid integer value of boolean type; expected 0 or 1, got %d", x))
 		}
 	}
+	// Output x in hexadecimal notation if x is positive, greater than or equal
+	// to 0x1000 and has a significantly lower entropy than decimal notation.
+
+	// Minimum difference between entropy of decimal and hexadecimal notation to
+	// output x in hexadecimal notation.
+	const minEntropyDiff = 0.2
+	// Maximum allowed entropy of hexadecimal notation to output x in hexadecimal
+	// notation.
+	//
+	// This is useful as some hex values, while lower entropy than their decimal
+	// counter-part do not improve readability.
+	//
+	// For instance, the decimal entropy of 7240739780546808700 is 9/10 = 0.9 and
+	// the hexadecimal entropy of 0x647C4677A2884B7C is 8/16 = 0.5. As such the
+	// entropy difference is 0.9-0.5 = 0.4, but the hexadecimal notation does not
+	// improve readability. Thus we add an upper bound on the hexadecimal entropy,
+	// and if the entropy is above this bound, output in decimal notation
+	// instead.
+	hexLength := len(c.X.Text(16))
+	maxHexEntropy := calcMaxHexEntropy(hexLength)
+	threshold := big.NewInt(0x1000) // 4096
+	// Check entropy if x >= 0x1000.
+	if c.X.Cmp(threshold) >= 0 {
+		hexentropy := hexEntropy(c.X)
+		decentropy := decimalEntropy(c.X)
+		if hexentropy <= maxHexEntropy + 0.01 && decentropy >= hexentropy+minEntropyDiff {
+			return "u0x" + strings.ToUpper(c.X.Text(16))
+		}
+	}
 	return c.X.String()
+}
+
+// ### [ Helper functions ] ####################################################
+
+// calcMaxHexEntropy returns the maximum allowed hexadecimal entropy based on
+// the length of x in hexadecimal notation.
+//
+//    maxHexEntropy = 0.0    length < 4
+//    maxHexEntropy = 0.5    length == 4 (2/4)
+//    maxHexEntropy = 0.4    length == 5 (2/5)
+//    maxHexEntropy = 0.34   length == 6 (2/6)
+//    maxHexEntropy = 0.43   length == 7 (3/7)
+//    maxHexEntropy = 0.38   length == 8 (3/8)
+//    maxHexEntropy = 0.34   length == 9 (3/9)
+//    maxHexEntropy = 0.3    length == 10 (3/10)
+//    maxHexEntropy = 0.37   length == 11 (4/11)
+//    maxHexEntropy = 0.34   length == 12 (4/12)
+//    maxHexEntropy = 0.31   length == 13 (4/13)
+//    maxHexEntropy = 0.29   length == 14 (4/14)
+//    maxHexEntropy = 0.27   length == 15 (4/15)
+//    maxHexEntropy = 0.25   length >= 16 (4/16)
+func calcMaxHexEntropy(length int) float64 {
+	if length > 16 {
+		length = 16
+	}
+	switch {
+	case length < 4:
+		return 0
+	case 4 <= length && length <= 6:
+		return 2.0 / float64(length)
+	case 7 <= length && length <= 10:
+		return 3.0 / float64(length)
+	// length >= 11
+	default:
+		return 4.0 / float64(length)
+	}
+}
+
+// hexEntropy returns the entropy of x when encoded in hexadecimal notation. The
+// entropy is in range (0.0, 1.0] and is determined by the number of unique hex
+// digits required to represent x in hexadecimal notation divided by the total
+// number of hex digits ignoring prefix (capped by base 16).
+//
+// For instance, the hexadecimal value 0x80000000 (2147483648 in decimal)
+// requires two unique hex digits to be represented in hexadecimal notation,
+// ignoring prefix; namely '0' and '8'.
+//
+// Hex digits of 0x80000000:
+//    0 0 0 0 0 0 0
+//    8
+//
+// The total number of hex digits in 0x80000000 is 8. Thus, the entropy of
+// 0x80000000 in hexadecimal notation is
+//
+//    unique_digits/total_digits
+//    = 2/8
+//    = 0.25
+func hexEntropy(x *big.Int) float64 {
+	const base = 16
+	return intEntropy(x, base)
+}
+
+// decimalEntropy returns the entropy of x when encoded in decimal notation. The
+// entropy is in range (0.0, 1.0] and is determined by the number of unique
+// decimal digits required to represent x in decimal notation divided by the
+// total number of digits (capped by base 10).
+//
+// For instance, the decimal value 2147483648 (0x80000000 in hex) requires seven
+// unique decimal digits to be represented in decimal notation; namely '1', '2',
+// '3', '4', '6', '7' and '8'.
+//
+// Decimal digits of 2147483648:
+//    1
+//    2
+//    3
+//    4 4 4
+//    6
+//    7
+//    8 8
+//
+// The total number of decimal digits in 2147483648 is 10. Thus, the entropy of
+// 2147483648 in decimal notation is
+//
+//    unique_digits/total_digits
+//    = 7/10
+//    = 0.7
+func decimalEntropy(x *big.Int) float64 {
+	const base = 10
+	return intEntropy(x, base)
+}
+
+// intEntropy returns the entropy of x when encoded in base notation. Base must
+// be between 2 and 62, inclusive. The entropy is in range (0.0, 1.0] and is
+// determined by the number of unique digits required to represent x in base
+// notation divided by the total number of digits (capped by base).
+func intEntropy(x *big.Int, base int) float64 {
+	if base < 2 || base > 62 {
+		panic(fmt.Errorf("invalid base; expected 2 <= base <= 62, got %d", base))
+	}
+	const maxBase = 62
+	var digits [maxBase]bool
+	s := x.Text(base)
+	// Locate unique digits.
+	for i := 0; i < len(s); i++ {
+		b := s[i]
+		if b == '-' {
+			// skip sign.
+			continue
+		}
+		d := digitValue(b)
+		digits[d] = true
+	}
+	// Count unique digits.
+	uniqueDigits := 0
+	for i := 0; i < base; i++ {
+		if digits[i] {
+			uniqueDigits++
+		}
+	}
+	length := len(s)
+	if length > base {
+		length = base
+	}
+	return float64(uniqueDigits) / float64(length)
+}
+
+// digitValue returns the integer value of the given digit byte. As defined by
+// *big.Int.Text, the digit uses the lower-case letters 'a' to 'z' for digit
+// values 10 to 35, and the upper-case letters 'A' to 'Z' for digit values 36 to
+// 61.
+func digitValue(b byte) int {
+	switch {
+	case '0' <= b && b <= '9':
+		return 0 + int(b-'0')
+	case 'a' <= b && b <= 'z':
+		return 10 + int(b-'a')
+	case 'A' <= b && b <= 'Z':
+		return 36 + int(b-'A')
+	default:
+		panic(fmt.Errorf("invalid digit byte; expected [0-9a-zA-Z], got %#U", b))
+	}
 }
---
+++ ir/constant/const_int_test.go
@@ -0,0 +1,43 @@
+package constant
+
+import (
+	"testing"
+
+	"github.com/llir/llvm/ir/types"
+)
+
+func TestIntIdent(t *testing.T) {
+	golden := []struct {
+		in   string
+		want string
+	}{
+		// integers < 0x1000 are always represented in decimal notation.
+		{in: "100", want: "100"},
+		{in: "256", want: "256"},
+		{in: "1000", want: "1000"},
+		{in: "4095", want: "4095"},
+		// integers >= 0x1000 are represented in hexadecimal notation if lower
+		// entropy than decimal notation.
+		{in: "4096", want: "u0x1000"},
+		{in: "2147483648", want: "u0x80000000"},
+		{in: "9218868437227405312", want: "u0x7FF0000000000000"},
+		{in: "1000000000000000000", want: "1000000000000000000"}, // hex would be u0xDE0B6B3A7640000
+		// negative integers are always represented in decimal notation.
+		{in: "-100", want: "-100"},
+		{in: "-256", want: "-256"},
+		{in: "-9218868437227405312", want: "-9218868437227405312"},
+		{in: "-1000000000000000000", want: "-1000000000000000000"},
+	}
+	for _, g := range golden {
+		c, err := NewIntFromString(types.I64, g.in)
+		if err != nil {
+			t.Errorf("unable to parse integer literal %q; %v", g.in, err)
+			continue
+		}
+		got := c.Ident()
+		if g.want != got {
+			t.Errorf("integer constant string mismatch; expected %q, got %q", g.want, got)
+			continue
+		}
+	}
+}
