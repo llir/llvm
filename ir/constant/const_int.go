3af4a2682877b52a98f211da1911b0dfd335818f
Robin Eklind <rnd0x00@gmail.com>
2020-01-08 01:27:12 +0100

    ir/constant: output int const in hex notation if more readable
    To improve readability of integer constants in LLVM IR output, a notion
    of "low entropy" is used to determine whether to output integer
    constants in decimal or hexadecimal notation.
    
    The main idea is to check how many unique digits are required for each
    representation and use the notation (hex or decimal) that requires the
    least.
    
    To illustrate this idea, here are a few examples from before and after
    this commit.
    
    Before: 2147483648
    After:  u0x80000000
    
    Before: 9218868437227405312
    After:  u0x7FF0000000000000
    
    For now, we have a threshold at 1000, so integer constants below 1000
    are always represented in decimal notation. This includes negative
    integer values.
    
    Fixes #125.

--- ir/constant/const_int.go
+++ ir/constant/const_int.go
@@ -127,5 +127,98 @@ func (c *Int) Ident() string {
 			panic(fmt.Errorf("invalid integer value of boolean type; expected 0 or 1, got %d", x))
 		}
 	}
+	// Output x in hexadecimal notation if x is positive, greater than or equal to
+	// 1000 and require less unique digits to be represented in hexadeciaml
+	// notation than decimal notation.
+	threashold := big.NewInt(1000)
+	if c.X.Cmp(threashold) >= 0 {
+		// Check entropy if x is >= 1000.
+		if decimalEntropy(c.X) > hexEntropy(c.X) {
+			return "u0x" + strings.ToUpper(c.X.Text(16))
+		}
+	}
 	return c.X.String()
+}
+
+// ### [ Helper functions ] ####################################################
+
+// hexEntropy returns the number of unique hex digits required to represent x in
+// hexadecimal notation.
+//
+// For instance, the hexadecimal value 0x80000000 (2147483648 in decimal)
+// requires two unique hex digits to be represented in hexadecimal notation,
+// ignoring prefix; namely '0' and '8'.
+//
+// Hex digits of 0x80000000:
+//    0 0 0 0 0 0 0
+//    8
+func hexEntropy(x *big.Int) int {
+	const base = 16
+	return intEntropy(x, base)
+}
+
+// decimalEntropy returns the number of unique decimal digits required to represent
+// x in decimal notation.
+//
+// For instance, the decimal value 2147483648 (0x80000000 in hex) requires seven
+// unique decimal digits to be represented in decimal notation; namely '1', '2',
+// '3', '4', '6', '7' and '8'.
+//
+// Decimal digits of 2147483648:
+//    1
+//    2
+//    3
+//    4 4 4
+//    6
+//    7
+//    8 8
+func decimalEntropy(x *big.Int) int {
+	const base = 10
+	return intEntropy(x, base)
+}
+
+// intEntropy returns the number of unique digits required to represent x in the
+// given base notation. Base must be between 2 and 62, inclusive.
+func intEntropy(x *big.Int, base int) int {
+	if base < 2 || base > 62 {
+		panic(fmt.Errorf("invalid base; expected 2 <= base <= 62, got %d", base))
+	}
+	const maxBase = 62
+	var digits [maxBase]bool
+	s := x.Text(base)
+	// Locate unique digits.
+	for i := 0; i < len(s); i++ {
+		b := s[i]
+		if b == '-' {
+			// skip sign.
+			continue
+		}
+		d := digitValue(b)
+		digits[d] = true
+	}
+	// Count unique digits.
+	entropy := 0
+	for i := 0; i < base; i++ {
+		if digits[i] {
+			entropy++
+		}
+	}
+	return entropy
+}
+
+// digitValue returns the integer value of the given digit byte. As defined by
+// *big.Int.Text, the digit uses the lower-case letters 'a' to 'z' for digit
+// values 10 to 35, and the upper-case letters 'A' to 'Z' for digit values 36 to
+// 61.
+func digitValue(b byte) int {
+	switch {
+	case '0' <= b && b <= '9':
+		return 0 + int(b-'0')
+	case 'a' <= b && b <= 'z':
+		return 10 + int(b-'a')
+	case 'A' <= b && b <= 'Z':
+		return 36 + int(b-'A')
+	default:
+		panic(fmt.Errorf("invalid digit byte; expected [0-9a-zA-Z], got %#U", b))
+	}
 }
