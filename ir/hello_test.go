5d437a7b07b519f78df38211e5bef999d546742d
John Jacobsen <eigenhombre@gmail.com>
2023-02-16 20:19:07 -0600

    Add `Hello World` example as a test.

---
+++ ir/hello_test.go
@@ -0,0 +1,37 @@
+package ir_test
+
+import (
+	"fmt"
+
+	"github.com/llir/llvm/ir"
+	"github.com/llir/llvm/ir/constant"
+	"github.com/llir/llvm/ir/types"
+)
+
+func Example_hello() {
+	// Create a new LLVM IR module.
+	m := ir.NewModule()
+	hello := constant.NewCharArrayFromString("Hello, world!\n\x00")
+	str := m.NewGlobalDef("str", hello)
+	// Add external function declaration of puts.
+	puts := m.NewFunc("puts", types.I32, ir.NewParam("", types.NewPointer(types.I8)))
+	main := m.NewFunc("main", types.I32)
+	entry := main.NewBlock("")
+	// Cast *[15]i8 to *i8.
+	zero := constant.NewInt(types.I64, 0)
+	gep := constant.NewGetElementPtr(hello.Typ, str, zero, zero)
+	entry.NewCall(puts, gep)
+	entry.NewRet(constant.NewInt(types.I32, 0))
+	fmt.Println(m)
+	// Output:
+	// @str = global [15 x i8] c"Hello, world!\0A\00"
+	//
+	// declare i32 @puts(i8* %0)
+	//
+	// define i32 @main() {
+	// 0:
+	// 	%1 = call i32 @puts(i8* getelementptr ([15 x i8], [15 x i8]* @str, i64 0, i64 0))
+	// 	ret i32 0
+	// }
+
+}
